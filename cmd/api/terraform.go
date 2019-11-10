68d5016bf22b5a6023e70eb1210628cf51ef08a7
Michael Guarino <mguarino46@gmail.com>
2019-11-10 01:05:59 -0500

    cli support for listing {chart,tf} installations

--- cmd/api/terraform.go
+++ cmd/api/terraform.go
@@ -11,6 +11,12 @@ type terraformResponse struct {
 	}
 }
 
+type terraformInstallationResponse struct {
+	TerraformInstallations struct {
+		Edges []TerraformInstallationEdge
+	}
+}
+
 var terraformQuery = fmt.Sprintf(`
 	query terraformQuery($id: ID!) {
 		terraform(repositoryId: $id, first: %d) {
@@ -24,10 +30,31 @@ var terraformQuery = fmt.Sprintf(`
 	%s
 `, pageSize, TerraformFragment)
 
+var terraformInstallationQuery = fmt.Sprintf(`
+	query TerraformInstallations($id: ID!) {
+		terraformInstallations(repositoryId: $id, first: %d) {
+			edges {
+				node {
+					...TerraformInstallationFragment
+				}
+			}
+		}
+	}
+	%s
+`, pageSize, TerraformInstallationFragment)
+
 func (client *Client) GetTerraforma(repoId string) ([]TerraformEdge, error) {
 	var resp terraformResponse
 	req := graphql.NewRequest(terraformQuery)
 	req.Var("id", repoId)
 	err := client.Run(req, &resp)
 	return resp.Terraform.Edges, err
+}
+
+func (client *Client) GetTerraformInstallations(repoId string) ([]TerraformInstallationEdge, error) {
+	var resp terraformInstallationResponse
+	req := graphql.NewRequest(terraformInstallationQuery)
+	req.Var("id", repoId)
+	err := client.Run(req, &resp)
+	return resp.TerraformInstallations.Edges, err
 }

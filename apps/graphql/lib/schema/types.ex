4261534838e2f62333d55838fc5cd6a230b8854f
Michael Guarino <mguarino46@gmail.com>
2019-12-24 17:32:28 -0500

    Add supported providers to tf/chart dependencies

--- apps/graphql/lib/schema/types.ex
+++ apps/graphql/lib/schema/types.ex
@@ -156,6 +156,7 @@ defmodule GraphQl.Schema.Types do
 
   object :dependencies do
     field :dependencies, list_of(:dependency)
+    field :providers, list_of(:provider)
     field :wirings, :wirings
   end
 

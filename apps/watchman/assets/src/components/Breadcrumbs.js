d9089cbedf45d960a05c60bb4a83948b825ffbf5
Michael Guarino <mguarino46@gmail.com>
2020-02-14 19:37:13 -0500

    Add breadcrumbs, broadcast command updates per stdio line

---
+++ apps/watchman/assets/src/components/Breadcrumbs.js
@@ -0,0 +1,42 @@
+import React, { useState, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+import { Box, Anchor, Text } from 'grommet'
+import { lookahead } from '../utils/array'
+
+export const BreadcrumbsContext = React.createContext({
+  breadcrumbs: [],
+  setBreadcrumbs: () => null
+})
+
+export function Breadcrumbs() {
+  let history = useHistory()
+  const {breadcrumbs} = useContext(BreadcrumbsContext)
+
+  if (breadcrumbs.length === 0) return null
+
+  const children = Array.from(lookahead(breadcrumbs, (crumb, next) => {
+    if (next.url) {
+      return [
+        <Anchor key={crumb.url} size='small' onClick={() => history.push(crumb.url)}>{crumb.text}</Anchor>,
+        <Text key={crumb.url + 'next'} size='small'>/</Text>
+      ]
+    }
+    return <Text key={crumb.url} size='small' color='dark-3'>{crumb.text}</Text>
+  })).flat()
+
+  return (
+    <Box flex={false} direction='row' gap='xsmall' pad={{horizontal: 'medium', vertical: 'small'}} border='bottom'>
+      {children}
+    </Box>
+  )
+}
+
+export default function BreadcrumbProvider({children}) {
+  const [breadcrumbs, setBreadcrumbs] = useState([])
+
+  return (
+    <BreadcrumbsContext.Provider value={{breadcrumbs, setBreadcrumbs}}>
+      {children}
+    </BreadcrumbsContext.Provider>
+  )
+}
--- apps/watchman/assets/src/utils/array.js
+++ apps/watchman/assets/src/utils/array.js
@@ -3,3 +3,10 @@ export function* reverse(array, mapper = (i) => i) {
     yield mapper(array[i])
   }
 }
+
+export function* lookahead(array, mapper = (i) => i) {
+  let len = array.length
+  for (let i = 0; i < len; i++) {
+    yield mapper(array[i], array[i + 1] || {})
+  }
+}

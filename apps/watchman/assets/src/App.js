95e7642e0eba4cf4772ae5ea8435d88ac3062a24
Michael Guarino <mguarino46@gmail.com>
2020-02-17 14:40:40 -0500

    Basic login form

--- apps/watchman/assets/src/App.js
+++ apps/watchman/assets/src/App.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom'
 import { Grommet } from 'grommet'
 import { DEFAULT_THEME } from './theme';
 import Watchman from './components/Watchman';
+import Login from './components/Login'
 
 export default function App() {
   return (
     <Grommet theme={DEFAULT_THEME}>
       <Switch>
+        <Route path='/login' component={Login} />
         <Route path='/' component={Watchman} />
       </Switch>
     </Grommet>

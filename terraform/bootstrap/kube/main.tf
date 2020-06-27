bd81265f9382921ce1843253279ce2e6c2b1ec09
Michael Guarino <mguarino46@gmail.com>
2020-06-27 19:59:00 -0400

    remove tiller config from terraform

--- terraform/bootstrap/kube/main.tf
+++ terraform/bootstrap/kube/main.tf
@@ -30,37 +30,6 @@ provider "kubernetes" {
   token = data.google_client_config.current.access_token
 }
 
-
-resource "kubernetes_service_account" "tiller" {
-  metadata {
-    name = "tiller"
-    namespace = "kube-system"
-  }
-  depends_on = [
-    null_resource.node_pool
-  ]
-}
-
-resource "kubernetes_cluster_role_binding" "tiller" {
-  metadata {
-    name = "tiller"
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-  depends_on = [
-    null_resource.node_pool
-  ]
-}
-
-
 ##
 # Finally tie everything back into our cluster
 ##

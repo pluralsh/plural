4e121e274aaaeb739653724c89127245e08ae2d2
michaelguarino <mguarino46@gmail.com>
2021-03-18 20:50:47 -0400

    start worker application properly in mixfile

--- apps/worker/lib/worker/conduit/subscribers/docker.ex
+++ apps/worker/lib/worker/conduit/subscribers/docker.ex
@@ -16,11 +16,15 @@ defmodule Worker.Conduit.Subscribers.Docker do
   def scan_image(image) do
     %{docker_repository: %{repository: repo} = dkr} = img = Core.Repo.preload(image, [docker_repository: :repository])
     %{publisher: %{owner: owner}} = Core.Repo.preload(repo, [publisher: :owner])
+
     registry_name  = "#{registry()}/#{repo.name}/#{dkr.name}"
+    image = "#{registry_name}:#{image.tag}"
+
     {:ok, registry_token} = Core.Services.Repositories.docker_token([:pull], registry_name, owner)
     env = [{"TRIVY_REGISTRY_TOKEN", registry_token}]
 
-    case System.cmd("trivy", ["image", "--output", "json", "#{registry_name}:#{image.tag}"], env: env) do
+    Logger.info "Scanning image #{image}"
+    case System.cmd("trivy", ["image", "--output", "json", image], env: env) do
       {output, 0} ->
         Jason.decode!(output)
         |> Enum.map(&TrivySource.to_vulnerability/1)
--- apps/worker/mix.exs
+++ apps/worker/mix.exs
@@ -18,17 +18,14 @@ defmodule Worker.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:lager, :logger],
+      mod: {Worker.Application, []}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:core, in_umbrella: true}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
     ]
   end
 end

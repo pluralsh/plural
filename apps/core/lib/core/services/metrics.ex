54a4cb2ccd304aaf581e3239ed072c2f60cf71aa
michaelguarino <mguarino46@gmail.com>
2021-04-16 00:28:43 -0400

    remove inspects

--- apps/core/lib/core/services/metrics.ex
+++ apps/core/lib/core/services/metrics.ex
@@ -9,7 +9,6 @@ defmodule Core.Services.Metrics do
   def docker_pull(repo, tag) do
     Docker.new(1, repository: repo, tag: tag)
     |> Influx.write()
-    |> IO.inspect()
   end
 
   def query_docker_pulls(repo, opts \\ []) do
@@ -18,7 +17,6 @@ defmodule Core.Services.Metrics do
 
     Docker.repo_query(offset, precision)
     |> Influx.query(params: %{repository: repo})
-    |> IO.inspect()
     |> response()
   end
 

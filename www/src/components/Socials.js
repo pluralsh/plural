77699271c416ee27cfff23f0e67861c499612f96
michaelguarino <mguarino46@gmail.com>
2022-01-22 23:11:57 -0500

    improve social links

--- www/src/components/Socials.js
+++ www/src/components/Socials.js
@@ -3,14 +3,14 @@ import { Box } from 'grommet'
 import { Github, Twitter } from 'grommet-icons'
 import { Discord } from 'forge-core'
 
-const ICON_SIZE = '15px'
-const BOX_SIZE  = '25px'
+const ICON_SIZE = '17px'
+const BOX_SIZE  = '28px'
 
 function SocialLink({icon, url}) {
   return (
     <a href={url} target='_blank'>
     <Box width={BOX_SIZE} height={BOX_SIZE} align='center' justify='center'
-         onClick={() => null} hoverIndicator='sidebarHover' round='xsmall'>
+         onClick={() => null} hoverIndicator='backgroundColor' round='xsmall'>
       {icon}
     </Box>
     </a>
@@ -19,7 +19,8 @@ function SocialLink({icon, url}) {
 
 export function SocialLinks() {
   return (
-    <Box flex={false} direction='row' gap='xsmall' border round='xsmall' pad='xsmall'>
+    <Box flex={false} background='sidebarHover' direction='row' gap='xsmall' 
+         border round='xsmall' pad={{horizontal: 'xsmall'}} height='32px' align='center'>
       <SocialLink 
         icon={<Github size={ICON_SIZE} />}
         url='https://github.com/pluralsh/plural' />

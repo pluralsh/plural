07765536298588922093d28a24c67a26670d828e
David Hérault <dherault@gmail.com>
2022-04-12 19:48:59 +0300

    Install design-system

--- www2/src/App.js
+++ www2/src/App.js
@@ -1,11 +1,22 @@
 import { ApolloProvider } from '@apollo/client'
+import { IntercomProvider } from 'react-use-intercom'
+import { Grommet } from 'grommet'
+import { theme } from 'pluralsh-design-system'
 
 import { client } from './helpers/client'
+import { INTERCOM_APP_ID } from './constants'
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      foo
+      <IntercomProvider appId={INTERCOM_APP_ID}>
+        <Grommet
+          full
+          theme={theme}
+        >
+          foo
+        </Grommet>
+      </IntercomProvider>
     </ApolloProvider>
   )
 }

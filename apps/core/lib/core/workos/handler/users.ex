72e6806b442ef4be910333e1a5600804602b6b32
michaeljguarino <mjg@plural.sh>
2022-04-26 17:26:44 -0400

    fallback to fetching users by email if no external_id for directory sync

--- apps/core/lib/core/workos/handler/users.ex
+++ apps/core/lib/core/workos/handler/users.ex
@@ -5,12 +5,17 @@ defimpl Core.WorkOS.Handler, for: [Core.WorkOS.Events.UserCreated, Core.WorkOS.E
   def handle(%@for{user: %{external_id: eid} = attrs, directory_id: id}) do
     %{account_id: aid} = Resources.get_directory(id)
 
-    case Core.Repo.get_by(User, external_id: eid) do
+    case fetch_user(attrs) do
       %User{} = user -> user
       _ -> %User{account_id: aid}
     end
     |> User.changeset(attrs)
     |> Ecto.Changeset.change(%{external_id: eid})
     |> Core.Repo.insert_or_update()
+  end
+
+  defp fetch_user(%{external_id: eid, email: email}) do
+    with nil <- Core.Repo.get_by(User, external_id: eid),
+      do: Core.Repo.get_by(User, email: email)
   end
 end

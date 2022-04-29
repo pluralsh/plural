c5ad5ada3cce2b4f738e1b1a8ac2886ebe87b6a8
David Hérault <dherault@gmail.com>
2022-04-29 19:59:27 +0300

    Redesign MagicLogin

--- www/src/components/constants.js
+++ www/src/components/constants.js
@@ -1,4 +1,5 @@
-export const SIDEBAR_WIDTH = '225px'
+export const SIDEBAR_WIDTH = '224px'
 export const PLURAL_ICON = `${process.env.PUBLIC_URL}/plrl-dark.png`
 export const PLURAL_WORD = `${process.env.PUBLIC_URL}/plural-white-word.png`
 export const PLURAL_MARK = `${process.env.PUBLIC_URL}/plural-logo.png`
+export const PLURAL_MARK_WHITE = `${process.env.PUBLIC_URL}/plural-white.png`

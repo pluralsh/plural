9efc506f71f28b319960d657108dae98070dde25
David Hérault <dherault@gmail.com>
2022-06-22 18:22:10 +0300

    Fix CloudWorkspace

--- www/src/components/shell/onboarding/cloud/CloudWorkspace.js
+++ www/src/components/shell/onboarding/cloud/CloudWorkspace.js
@@ -57,19 +57,19 @@ function CloudWorkspace() {
             theme={{
               // Theme extension to give the Input a custom end icon
               Input: {
-                // Root: [
-                //   {
-                //     // height: 34,
-                //     // paddingTop: 0,
-                //     // paddingBottom: 0,
-                //     // paddingLeft: 8,
-                //     // paddingRight: 0,
-                //   },
-                // ],
+                Root: [
+                  {
+                    // height: 34,
+                    // paddingTop: 0,
+                    // paddingBottom: 0,
+                    // paddingLeft: 8,
+                    paddingRight: 0,
+                  },
+                ],
                 EndIcon: [
                   {
                     height: '100%',
-                    margin: 0,
+                    marginLeft: 0,
                   },
                 ],
               },

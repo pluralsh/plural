0db034ed22eaba61f1a6199cbc936d81cdd1f953
David Hérault <dherault@gmail.com>
2022-06-23 23:13:19 +0300

    Remove debug token

--- www/src/components/shell/debug-tokens.js
+++ www/src/components/shell/debug-tokens.js
@@ -1,6 +1,6 @@
 // START <<Remove this after dev>>
 export const DEBUG_SCM_TOKENS = {
   GITLAB: '',
-  GITHUB: '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',
+  GITHUB: '',
 }
 // END <<Remove this after dev>>

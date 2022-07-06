e59bc15f9de0eca00c0d4bfe782438b0a5481277
Sebastian Florek <sebastian@plural.sh>
2022-07-06 19:34:04 +0200

    Remove scrollbar styling and bump design system to use global styling

--- www/src/index.js
+++ www/src/index.js
@@ -3,8 +3,6 @@ import { render } from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 import App from './App'
 
-import './index.css'
-
 render(
   <App />,
   document.getElementById('root')

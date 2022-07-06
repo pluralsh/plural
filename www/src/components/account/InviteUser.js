ff247d159200540cdfce082dc4acf1dfb4841a93
michaeljguarino <mjg@plural.sh>
2022-07-06 12:12:08 -0400

    More Accounts Pages fixes
    Continue wrangling with modal sizing, validate invite emails are correctly formatted

--- www/src/components/account/InviteUser.js
+++ www/src/components/account/InviteUser.js
@@ -9,11 +9,12 @@ import { useMutation } from '@apollo/client'
 
 import { inviteLink } from '../accounts/CreateInvite'
 import { CREATE_INVITE } from '../accounts/queries'
+import { GqlError } from '../utils/Alert'
 
 export function InviteUser() {
   const [open, setOpen] = useState(false)
   const [email, setEmail] = useState('')
-  const [mutation, { loading, data }] = useMutation(CREATE_INVITE, {
+  const [mutation, { loading, data, error }] = useMutation(CREATE_INVITE, {
     variables: { attributes: { email } },
   })
 
@@ -38,6 +39,12 @@ export function InviteUser() {
           onChange={({ target: { value } }) => setEmail(value)}
           label="Email address"
         />
+        {error && (
+          <GqlError
+            error={error}
+            header="Failed to invite user"
+          />
+        )}
         {invite && <Codeline marginTop="small">{inviteLink(invite)}</Codeline>}
         <ModalActions>
           <Button

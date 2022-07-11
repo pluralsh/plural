5100066a654816f15a89030580c33c2291c27dae
Marcin Maciaszczyk <marcin@plural.sh>
2022-07-11 14:12:55 +0200

    Redesign packages tabs

--- www/src/components/repository/RepositoryPackages.js
+++ www/src/components/repository/RepositoryPackages.js
@@ -1,63 +1,30 @@
-import { Link, Outlet, useLocation } from 'react-router-dom'
-import { Div, Flex, H2 } from 'honorable'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { Flex } from 'honorable'
 
-function RepositoryPackagesTab({ to, label, active }) {
-  const hoverStyle = {
-    '&:hover': {
-      backgroundColor: 'fill-one',
-    },
-  }
-
-  return (
-    <Div
-      as={Link}
-      to={to}
-      px={1}
-      pt={0.333}
-      pb={0.25}
-      mr={1}
-      color="text"
-      textDecoration="none"
-      fontWeight={600}
-      borderTopLeftRadius={4}
-      borderTopRightRadius={4}
-      borderBottom={`2px solid ${active ? 'primary' : 'transparent'}`}
-      {...hoverStyle}
-    >
-      {label}
-    </Div>
-  )
-}
+import { ButtonGroup } from '../utils/ButtonGroup'
 
 function RepositoryPackages() {
   const { pathname } = useLocation()
+  const navigate = useNavigate()
+  const tabToUrl = {
+    'Helm Charts': 'helm',
+    'Terraform Modules': 'terraform',
+    'Docker Repositories': 'docker',
+  }
+  const currentPath = pathname.substring(pathname.lastIndexOf('/') + 1)
+  const defaultTab = Object.keys(tabToUrl).find(key => tabToUrl[key] === currentPath)
+  const changeTab = tab => navigate(tabToUrl[tab])
 
   return (
     <Flex
       direction="column"
       height="100%"
     >
-      <H2 subtitle1>
-        Packages
-      </H2>
-      <Flex
-        mt={2}
-        borderBottom="1px solid border"
-      >
-        <RepositoryPackagesTab
-          label="Helm Charts"
-          to="helm"
-          active={pathname.endsWith('helm')}
-        />
-        <RepositoryPackagesTab
-          label="Terraform Modules"
-          to="terraform"
-          active={pathname.endsWith('terraform')}
-        />
-        <RepositoryPackagesTab
-          label=" Docker Repositories"
-          to="docker"
-          active={pathname.endsWith('docker')}
+      <Flex justifyContent="flex-end">
+        <ButtonGroup
+          tabs={Object.keys(tabToUrl)}
+          default={defaultTab}
+          onChange={changeTab}
         />
       </Flex>
       <Flex

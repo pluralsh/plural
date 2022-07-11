8b2c64f5c849072bae184fcf5f2869f03b87889c
Sebastian Florek <sebastian@plural.sh>
2022-07-11 10:26:09 +0200

    Use requested scopes for the oauth consent flow

--- www/src/components/oidc/OAuthConsent.js
+++ www/src/components/oidc/OAuthConsent.js
@@ -2,30 +2,29 @@ import { useLocation } from 'react-router-dom'
 import { useMutation, useQuery } from '@apollo/client'
 import { Button } from 'forge-core'
 import queryString from 'query-string'
-
 import { Transaction } from 'grommet-icons'
-
 import { Box, Text } from 'grommet'
 
 import { LoginPortal } from '../users/MagicLogin'
 import { GqlError } from '../utils/Alert'
-
 import { PLURAL_MARK_WHITE } from '../constants'
 import { RepoIcon } from '../repos/Repositories'
 import { LoopingLogo } from '../utils/AnimatedLogo'
 
-import { GET_CONSENT, OAUTH_CONSENT } from './queries'
+import { GET_OIDC_CONSENT, OAUTH_CONSENT } from './queries'
 
 export function OAuthConsent() {
   const location = useLocation()
   const { consent_challenge: challenge } = queryString.parse(location.search)
+  const { data } = useQuery(GET_OIDC_CONSENT, { variables: { challenge } })
+  const repository = data?.oidcConsent?.repository
+  const consent = data?.oidcConsent?.consent
   const [mutation, { loading, error }] = useMutation(OAUTH_CONSENT, {
-    variables: { challenge, scopes: ['profile', 'openid'] },
+    variables: { challenge, scopes: consent?.requestedScope || ['profile', 'openid'] },
     onCompleted: ({ oauthConsent: { redirectTo } }) => {
       window.location = redirectTo
     },
   })
-  const { data } = useQuery(GET_CONSENT, { variables: { challenge } })
 
   if (!data) {
     return (
@@ -36,8 +35,6 @@ export function OAuthConsent() {
       </Box>
     )
   }
-
-  const { oauthConsent } = data
 
   return (
     <LoginPortal>
@@ -59,11 +56,11 @@ export function OAuthConsent() {
           />
           <Transaction size="15px" />
           <RepoIcon
-            repo={oauthConsent}
+            repo={repository}
             size="60px"
           />
         </Box>
-        <Text size="medium">{oauthConsent.name} would like access to your profile</Text>
+        <Text size="medium">{repository.name} would like access to your profile</Text>
         <Box
           width="300px"
           pad={{ vertical: 'small' }}

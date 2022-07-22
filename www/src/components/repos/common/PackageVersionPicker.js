14c67c7267efad444a942ffacc2382fd15ac4e03
Marcin Maciaszczyk <marcin@plural.sh>
2022-07-22 15:04:21 +0200

    Add comment

--- www/src/components/repos/common/PackageVersionPicker.js
+++ www/src/components/repos/common/PackageVersionPicker.js
@@ -5,6 +5,7 @@ import { MenuItem, Select, Text } from 'honorable'
 import { Chip } from 'pluralsh-design-system'
 
 // TODO: Implement view more functionality as at the moment it loads only the first page.
+// TODO: Show only 6 elements inside select component and add scroll (to be done globally).
 export function PackageVersionPicker({ edges, installed, version, setVersion }) {
 
   return (

f25281ebb3cfb87b3a0d7a722abe44b54d058d11
Marcin Maciaszczyk <marcin@plural.sh>
2022-08-01 15:34:23 +0200

    Update migration

--- apps/core/priv/repo/migrations/20220801132654_add_repository_community.exs
+++ apps/core/priv/repo/migrations/20220801132654_add_repository_community.exs
@@ -2,6 +2,8 @@ defmodule Core.Repo.Migrations.AddRepositoryCommunity do
   use Ecto.Migration
 
   def change do
-
+    alter table(:repositories) do
+      add :community, :map
+    end
   end
 end

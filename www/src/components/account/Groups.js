c25a1d09d0a518eb9da244cbf43f5d958e43f859
Marcin Maciaszczyk <marcin@plural.sh>
2022-08-05 10:16:18 +0200

    Fix copy paste

--- www/src/components/account/Groups.js
+++ www/src/components/account/Groups.js
@@ -181,7 +181,7 @@ function GroupsInner({ q }) {
           placeholder={Placeholder}
         />
       ) : (
-        <EmptyState message={isEmpty(q) ? "Looks like you don't have any groups yet." : `No roles found for ${q}`}>
+        <EmptyState message={isEmpty(q) ? "Looks like you don't have any groups yet." : `No groups found for ${q}`}>
           <CreateGroup q={q} />
         </EmptyState>
       )}

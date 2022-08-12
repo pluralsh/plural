cea127814b00dcbd76f2d18e482649fe360e8928
DavidSpek <vanderspek.david@gmail.com>
2022-08-12 14:27:04 +0200

    init elixir changes
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>

--- mix.exs
+++ mix.exs
@@ -1,12 +1,45 @@
 defmodule Plural.MixProject do
   use Mix.Project
 
-  @vsn File.read!("VERSION")
+  defp version do
+    case :file.consult('hex_metadata.config') do
+      {:ok, data} ->
+        {"version", version} = List.keyfind(data, "version", 0)
+        version
+      _ ->
+        version =
+          case System.cmd("git", ~w[describe --dirty=+dirty]) do
+            {version, 0} ->
+              String.trim_leading(String.trim(version), "v")
+
+            {_, code} ->
+              Mix.shell().error("Git exited with code #{code}, falling back to 0.0.0")
+
+              "0.0.0"
+          end
+
+        case Version.parse(version) do
+          {:ok, %Version{pre: ["pre" <> _ | _]} = version} ->
+            to_string(version)
+
+          {:ok, %Version{pre: []} = version} ->
+            to_string(version)
+
+          {:ok, %Version{patch: patch, pre: pre} = version} ->
+            to_string(%{version | patch: patch + 1, pre: ["dev" | pre]})
+
+          :error ->
+            Mix.shell().error("Failed to parse #{version}, falling back to 0.0.0")
+
+            "0.0.0"
+        end
+    end
+  end
 
   def project do
     [
       apps_path: "apps",
-      version: @vsn,
+      version: version(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]

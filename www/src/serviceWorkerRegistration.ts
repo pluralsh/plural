391bbb01989934fd5f99b2fc9da67f2e4281c34b
Sebastian Florek <sebastian@plural.sh>
2022-08-16 17:42:43 +0200

    Initial commit

---
+++ www/src/serviceWorkerRegistration.ts
@@ -0,0 +1,112 @@
+const isProduction = process.env.NODE_ENV === 'production'
+const url = process.env.PUBLIC_URL || ''
+const isServiceWorkerAvailable = 'serviceWorker' in navigator
+const serviceWorkerURL = `${url}/service-worker.js`
+const isLocalhost = Boolean(window.location.hostname === 'localhost'
+  // [::1] is the IPv6 localhost address.
+  || window.location.hostname === '[::1]'
+  // ignore on preview branches
+  || window.location.hostname.endsWith('web.app')
+  // 127.0.0.0/8 are considered localhost for IPv4.
+  || window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/))
+
+type Config = {
+  onSuccess?: (registration: ServiceWorkerRegistration) => void
+  onUpdate?: (registration: ServiceWorkerRegistration) => void
+}
+
+export const register = (config: Config): void => {
+  const publicURL = new URL(url, window.location.href)
+
+  if (!isProduction || !isServiceWorkerAvailable) {
+    return
+  }
+
+  if (publicURL.origin !== window.location.origin) {
+    // Our service worker won't work if PUBLIC_URL is on a different origin
+    // from what our page is served on. This might happen if a CDN is used to
+    // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+    return
+  }
+
+  window.addEventListener('load', onLoad(config))
+}
+
+const onLoad = config => async () => {
+  if (!isLocalhost) {
+    registerServiceWorker(config)
+
+    return
+  }
+
+  const hasServiceWorker = await checkServiceWorker()
+
+  if (!hasServiceWorker) {
+    navigator.serviceWorker.ready.then(registration => registration.unregister().then(() => window.location.reload()))
+
+    return
+  }
+
+  registerServiceWorker(config)
+
+  // Add some additional logging to localhost, pointing developers to the
+  // service worker/PWA documentation.
+  navigator.serviceWorker.ready.then(() => {
+    console.log('This web app is being served cache-first by a service '
+      + 'worker. To learn more, visit https://cra.link/PWA')
+  })
+}
+
+const checkServiceWorker = (): Promise<boolean | void> => fetch(serviceWorkerURL, { headers: { 'Service-Worker': 'script' } }).then(response => {
+  const contentType = response.headers.get('content-type')
+
+  return !(response.status === 404 || contentType?.indexOf('javascript') === -1)
+}).catch(() => console.log('No internet connection found. App is running in offline mode.'))
+
+const registerServiceWorker = config => {
+  navigator.serviceWorker.register(serviceWorkerURL).then((registration: ServiceWorkerRegistration) => {
+    registration.onupdatefound = onServiceWorkerUpdateFound(registration, config)
+  })
+}
+
+const onServiceWorkerUpdateFound = (registration: ServiceWorkerRegistration, config) => () => {
+  const { installing } = registration
+
+  if (!installing) return
+
+  installing.onstatechange = onServiceWorkerStateChange(registration, config)
+}
+
+const onServiceWorkerStateChange = (registration: ServiceWorkerRegistration, config: Config) => () => {
+  const { installing } = registration
+
+  if (installing?.state !== 'installed') return
+
+  if (!navigator.serviceWorker.controller) {
+    // At this point, everything has been precached.
+    // It's the perfect time to display a
+    // "Content is cached for offline use." message.
+    console.log('Content is cached for offline use.')
+
+    // Execute callback
+    if (config?.onSuccess) {
+      config.onSuccess(registration)
+    }
+
+    return
+  }
+
+  console.log('New content is available and will be used when all '
+    + 'tabs for this page are closed. See https://cra.link/PWA.')
+
+  // Execute callback
+  if (config?.onUpdate) {
+    config.onUpdate(registration)
+  }
+}
+
+export const unregister = () => {
+  if (!isServiceWorkerAvailable) return
+
+  navigator.serviceWorker.ready.then((registration: ServiceWorkerRegistration) => registration.unregister()).catch(error => console.error(error.message))
+}

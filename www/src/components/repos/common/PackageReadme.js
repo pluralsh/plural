8f627020ca2b80f6079a1678522306e78d2b1bf3
Marcin Maciaszczyk <marcin9yk@icloud.com>
2022-08-18 10:52:38 +0200

    Update empty readme message

--- www/src/components/repos/common/PackageReadme.js
+++ www/src/components/repos/common/PackageReadme.js
@@ -6,7 +6,7 @@ import { Markdown, PageTitle } from 'pluralsh-design-system'
 
 export default function PackageReadme() {
   const { currentHelmChart, currentTerraformChart } = useOutletContext()
-  const readme = (currentHelmChart || currentTerraformChart)?.readme || 'No readme found.'
+  const readme = (currentHelmChart || currentTerraformChart)?.readme || 'This package does not have a Readme yet.'
 
   return (
     <Box

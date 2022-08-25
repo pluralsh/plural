2106c046329c1071257a33eb188478c5d8a9e3b8
David Hérault <dherault@gmail.com>
2022-08-25 19:17:15 +0200

    Add dummy name to email helper

--- apps/email/lib/email/helper.ex
+++ apps/email/lib/email/helper.ex
@@ -5,7 +5,7 @@ defmodule Email.Helper do
   def confirm_email(email) do
     case Users.get_user_by_email(email) do
       %User{} -> Users.create_reset_token(%{type: :email, email: email})
-      _ -> Users.create_user(%{email: email, password: Ecto.UUID.generate()})
+      _ -> Users.create_user(%{email: email, password: Ecto.UUID.generate(), name: email})
     end
   end
 

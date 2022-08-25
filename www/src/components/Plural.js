4d5f087daef948d9073cceccaf5fd6078edb5064
Klink <85062+dogmar@users.noreply.github.com>
2022-08-25 08:22:46 -0700

    Restore order of Account tabs and default to ‘Account attributes’ on page load

--- www/src/components/Plural.js
+++ www/src/components/Plural.js
@@ -328,7 +328,7 @@ export function PluralInner() {
                 element={(
                   <Navigate
                     replace
-                    to="users"
+                    to="edit"
                   />
                 )}
               />

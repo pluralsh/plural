2ed2c6d81087a0debbe95f638fb056093be848e6
Marcin Maciaszczyk <marcin9yk@icloud.com>
2022-09-08 15:02:51 +0200

    Remove unused SidebarTabs component

--- www/src/components/utils/SidebarTabs.js
+++ www/src/components/utils/SidebarTabs.js
@@ -1,27 +1,6 @@
 import { Div, Flex } from 'honorable'
 import { Tab } from 'pluralsh-design-system'
 
-export function SidebarTabs({ width = 250, children }) {
-  return (
-    <Flex
-      px={1}
-      py={1}
-      width={width}
-      flexShrink={0}
-      direction="column"
-    >
-      <Div
-        borderRight="1px solid border"
-      />
-      { children }
-      <Div
-        flexGrow={1}
-        borderRight="1px solid border"
-      />
-    </Flex>
-  )
-}
-
 export function Tabs({ tabs, tab, setTab }) {
   return (
     <Flex>

2de0c9ac5d3309bce2dc26c06a5741527d6bac1f
michaeljguarino <mjg@plural.sh>
2022-09-12 10:12:07 -0400

    Add all supportable aws regions to cloud shell
    It looks like we were only using a subset of the usable regions, this should bring parity with the cli

--- www/src/components/shell/onboarding/cloud/aws.js
+++ www/src/components/shell/onboarding/cloud/aws.js
@@ -5,10 +5,28 @@ import { FormField, Input } from 'pluralsh-design-system'
 import { stringExists } from '../../validation'
 
 const REGIONS = [
+  'af-south-1',
+  'eu-north-1',
+  'ap-south-1',
+  'eu-west-3',
+  'eu-west-2',
+  'eu-south-1',
+  'eu-west-1',
+  'ap-northeast-3',
+  'ap-northeast-2',
+  'me-south-1',
+  'ap-northeast-1',
+  'sa-east-1',
+  'ca-central-1',
+  'ap-east-1',
+  'ap-southeast-1',
+  'ap-southeast-2',
+  'eu-central-1',
+  'ap-southeast-3',
   'us-east-1',
   'us-east-2',
+  'us-west-1',
   'us-west-2',
-  'eu-west-1',
 ]
 
 export const AWS_VALIDATIONS = [

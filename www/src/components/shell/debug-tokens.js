8f84881700ba66300d0bf74f7bb4606de57f54cd
michaeljguarino <mjg@plural.sh>
2022-09-29 16:46:39 -0400

    rm token

--- www/src/components/shell/debug-tokens.js
+++ www/src/components/shell/debug-tokens.js
@@ -2,6 +2,6 @@
 // START <<Remove this after dev>>
 export const DEBUG_SCM_TOKENS = {
   GITLAB: '',
-  GITHUB: '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',
+  GITHUB: '',
 }
 // END <<Remove this after dev>>

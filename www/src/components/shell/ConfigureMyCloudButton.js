46cc924bba2fe5f97a26be91b58dab8af7cc0782
Marcin Maciaszczyk <marcin9yk@icloud.com>
2022-10-25 13:14:12 +0200

    Show pulse animation only when using checklist ref

--- www/src/components/shell/ConfigureMyCloudButton.js
+++ www/src/components/shell/ConfigureMyCloudButton.js
@@ -4,15 +4,17 @@ import { CurrentUserContext } from 'components/login/CurrentUser'
 import { OnboardingStatus } from 'components/profile/types'
 import { Button, CloudIcon } from 'pluralsh-design-system'
 import { useContext, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 import { DELETE_DEMO_PROJECT_QUERY, POLL_DEMO_PROJECT_QUERY } from './queries'
 
 export default function ConfigureMyCloudButton() {
   const [open, setOpen] = useState(false)
+  const [searchParams] = useSearchParams()
   const navigate = useNavigate()
   const me = useContext(CurrentUserContext)
   const isOnboarded = me.onboarding === OnboardingStatus.ONBOARDED
+  const isChecklistRef = searchParams.get('ref') === 'checklist'
   const { data } = useQuery(POLL_DEMO_PROJECT_QUERY, { pollInterval: 10000 })
   const [mutation, { loading, error }] = useMutation(DELETE_DEMO_PROJECT_QUERY, {
     onCompleted: () => {
@@ -27,7 +29,7 @@ export default function ConfigureMyCloudButton() {
     <>
       <Button
         small
-        pulse
+        pulse={isChecklistRef}
         startIcon={<CloudIcon />}
         onClick={() => setOpen(true)}
       >

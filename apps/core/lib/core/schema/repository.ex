29eae82516a3c8f1db4b7810d0ba3a80ca6557e2
test <test@plural.com>
2022-10-31 15:52:58 -0400

    json encode the community struct

--- apps/core/lib/core/schema/repository.ex
+++ apps/core/lib/core/schema/repository.ex
@@ -98,6 +98,13 @@ defmodule Core.Schema.Repository do
     end
   end
 
+  defimpl Jason.Encoder, for: __MODULE__.Community do
+    def encode(struct, opts) do
+      Piazza.Ecto.Schema.mapify(struct)
+      |> Jason.Encode.map(opts)
+    end
+  end
+
   def for_category(query \\ __MODULE__, category) do
     from(r in query, where: r.category == ^category)
   end

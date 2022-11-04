96ab14268ba2051bedefbd3254ce85f84fb1b21f
David van der Spek <vanderspek.david@gmail.com>
2022-11-04 18:36:34 +0100

    add dedicated node groups for Plural
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- plural/terraform/aws/variables.tf
+++ plural/terraform/aws/variables.tf
@@ -51,3 +51,64 @@ EOF
 variable "shell_subnet_ids" {
   type = list(string)
 }
+
+variable "node_role_arn" {
+  type = string
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "node_groups_defaults" {
+  description = "map of maps of node groups to create. See \"`node_groups` and `node_groups_defaults` keys\" section in README.md for more details"
+  type        = any
+  default = {
+    desired_capacity = 0
+    min_capacity = 0
+    max_capacity = 3
+
+    instance_types = ["t3.large", "t3a.large"]
+    disk_size = 50
+    ami_release_version = "1.21.14-20220824"
+    force_update_version = true
+    ami_type = "AL2_x86_64"
+    k8s_labels = {}
+    k8s_taints = []
+  }
+}
+
+variable "single_az_node_groups" {
+  type = any
+  default = {
+    plural_small = {
+      name = "plural-small"
+      capacity_type = "ON_DEMAND"
+      min_capacity = 0
+      max_capacity = 9
+      desired_capacity = 0
+      instance_types = ["t3.large", "t3a.large"]
+      k8s_labels = {
+        "plural.sh/capacityType" = "ON_DEMAND"
+        "plural.sh/performanceType" = "SUSTAINED"
+        "plural.sh/scalingGroup" = "plural-small"
+      }
+      k8s_taints = [
+        {
+          key = "plural.sh/pluralReserved"
+          value = "true"
+          effect = "NO_SCHEDULE"
+        }
+      ]
+    }
+  }
+  description = "Node groups to add to your cluster. A single managed node group will be created in each availability zone."
+}
+
+variable "private_subnets" {
+  description = "A list of private subnets for the EKS worker groups."
+  type        = list(any)
+  default     = []
+}

d5ea7e63b5a5eef841baa26efd1fe4c9e2fccb1c
David Hérault <dherault@gmail.com>
2022-11-15 04:25:13 +0100

    Edit index.html

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import GlobalPolyFill from "@esbuild-plugins/node-globals-polyfill";
 
-console.log('GlobalPolyFill',GlobalPolyFill );
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [

d8276cb4188eafbb14e3be9fb52849f28e635e9a
David Hérault <dherault@gmail.com>
2022-11-15 03:04:22 +0100

    Deprecate Alert

--- www/src/components/publisher/CreatePublisherModal.tsx
+++ www/src/components/publisher/CreatePublisherModal.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Button, Flex, Modal } from 'honorable'
-import { Alert, FormField, Input } from 'pluralsh-design-system'
+import { FormField, Input } from 'pluralsh-design-system'
 import { useMutation } from '@apollo/client'
 import { useFilePicker } from 'react-sage'
 import isArray from 'lodash/isArray'
 
-import { isValidUrl } from 'utils/string'
-
+import { isValidUrl } from '../../utils/string'
 import { generatePreview } from '../../utils/file'
 
 import IconUploadPreview from '../utils/IconUploadPreview'
+import { Alert, AlertStatus } from '../utils/Alert'
 
 import { CREATE_PUBLISHER_MUTATION } from './queries'
 
@@ -257,9 +257,11 @@ function CreatePublisherModal({ open, onClose }: CreatePublisherModalProps) {
         )}
       </Flex>
       {!!preMutationError && (
-        <Alert severity="error">
-          {preMutationError}
-        </Alert>
+        <Alert
+          status={AlertStatus.ERROR}
+          hearder="Error"
+          description={preMutationError}
+        />
       )}
     </Modal>
   )

cf305e8106410bf8d7a9e59cd341a81d6cd169d0
michaeljguarino <mguarino46@gmail.com>
2022-11-30 14:56:31 -0500

    Add call to setupShell before entering cloud shell
    This will among other things, allow us to properly message setup errors

--- www/src/components/shell/Terminal.tsx
+++ www/src/components/shell/Terminal.tsx
@@ -1,8 +1,10 @@
-import { useQuery } from '@apollo/client'
+import { useMutation, useQuery } from '@apollo/client'
 import { Flex } from 'honorable'
-import { LoopingLogo } from '@pluralsh/design-system'
+import { Banner, LoopingLogo } from '@pluralsh/design-system'
 
-import { CLOUD_SHELL_QUERY } from './queries'
+import { useEffect } from 'react'
+
+import { CLOUD_SHELL_QUERY, SETUP_SHELL } from './queries'
 
 import { ShellStatus } from './onboarding/ShellStatus'
 import OnboardingWrapper from './onboarding/OnboardingWrapper'
@@ -11,21 +13,57 @@ import { JoinCommunityCard } from './onboarding/JoinCommunityCard'
 import TerminalThemeProvider from './TerminalThemeProvider'
 import Shell from './Shell'
 
+function Loading() {
+  return (
+    <Flex
+      grow={1}
+      align="center"
+      justify="center"
+    >
+      <LoopingLogo />
+    </Flex>
+  )
+}
+
+function TerminalWrapper({ shell }) {
+  const [mutation, { error, data }] = useMutation(SETUP_SHELL)
+
+  useEffect(() => {
+    mutation()
+  }, [])
+
+  if (error) {
+    return (
+      <OnboardingWrapper stepIndex={3}>
+        <Banner
+          heading="Failed to set up shell"
+          severity="error"
+        >
+          {error.graphQLErrors[0].message}
+        </Banner>
+        <JoinCommunityCard />
+      </OnboardingWrapper>
+    )
+  }
+
+  if (!data?.id) {
+    return <Loading />
+  }
+
+  return (
+    <TerminalThemeProvider>
+      <Shell shell={shell} />
+    </TerminalThemeProvider>
+  )
+}
+
 export function Terminal() {
   const { data } = useQuery(CLOUD_SHELL_QUERY, { pollInterval: 5000, fetchPolicy: 'cache-and-network' })
   const { shell } = data || {}
   const { alive, status } = shell || {}
 
   if (!status) {
-    return (
-      <Flex
-        grow={1}
-        align="center"
-        justify="center"
-      >
-        <LoopingLogo />
-      </Flex>
-    )
+    return <Loading />
   }
 
   if (!alive) {
@@ -37,9 +75,5 @@ export function Terminal() {
     )
   }
 
-  return (
-    <TerminalThemeProvider>
-      <Shell shell={shell} />
-    </TerminalThemeProvider>
-  )
+  return <TerminalWrapper shell={shell} />
 }

52020b75006f3cbfc587e9d4ec61000814650096
David Hérault <dherault@gmail.com>
2022-12-14 21:18:34 +0100

    Enhance RoadmapIssue

--- www/src/components/roadmap/RoadmapIssue.tsx
+++ www/src/components/roadmap/RoadmapIssue.tsx
@@ -1,4 +1,5 @@
-import { Flex, P } from 'honorable'
+import moment from 'moment'
+import { Div, Flex, P } from 'honorable'
 import { Button, Chip, GitHubLogoIcon } from '@pluralsh/design-system'
 
 import { IssueType } from './types'
@@ -17,34 +18,57 @@ function RoadmapIssue({ issue }: RoadmapIssuePropsType) {
   return (
     <Flex
       align="center"
-      gap="medium"
+      borderBottom="1px solid border"
+      padding="medium"
     >
       <Flex
         direction="column"
         minWidth={0}
+        gap="xxsmall"
       >
         <Flex
           align="center"
           gap="small"
           minWidth={0}
         >
-          <P {...ellipsis}>
+          <P
+            body1
+            fontWeight="bold"
+            {...ellipsis}
+          >
             {issue.title}
           </P>
-          <P>
-            {issue.createdAt}
+          <P
+            overline
+            flexShrink={0}
+          >
+            {moment(issue.createdAt).fromNow(true)}
           </P>
         </Flex>
-        <P {...ellipsis}>
-          {prepareIssueBody(issue.body)}
+        <P
+          body2
+          color="text-light"
+          {...ellipsis}
+        >
+          {prepareIssueBody(issue.body) || '\u00A0'}
         </P>
       </Flex>
-      <Chip size="large">
+      <Div flexGrow={1} />
+      <Chip
+        size="large"
+        flexShrink={0}
+        marginLeft="medium"
+      >
         In progress
       </Chip>
       <Button
+        as="a"
+        href={issue.url}
+        target="_blank"
         secondary
         endIcon={<GitHubLogoIcon />}
+        flexShrink={0}
+        marginLeft="medium"
       >
         View on GitHub
       </Button>
@@ -53,10 +77,17 @@ function RoadmapIssue({ issue }: RoadmapIssuePropsType) {
 }
 
 const summaryText = '## Summary'
+const commentText = '<!-- A brief description of the issue and what you expect to happen instead -->'
+const imageRegex = /<img .*>/gm
 
 function prepareIssueBody(body: string) {
   const indexOfSummary = body.indexOf(summaryText)
-  const text = body.slice(indexOfSummary !== -1 ? indexOfSummary + summaryText.length : 0, 21)
+  let text = body.replaceAll(commentText, '')
+
+  text = text.replaceAll(imageRegex, '')
+  text = indexOfSummary !== -1
+    ? text.slice(indexOfSummary + summaryText.length, indexOfSummary + summaryText.length + 100)
+    : text.slice(0, 100)
 
   return text.split('\n').join(' ')
 }

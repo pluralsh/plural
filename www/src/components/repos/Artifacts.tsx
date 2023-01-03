65e2b9bbce1d971db389a476c300a9140f481e51
David Hérault <dherault@gmail.com>
2023-01-03 19:45:43 +0100

    Lint

--- www/src/components/repos/Artifacts.tsx
+++ www/src/components/repos/Artifacts.tsx
@@ -305,7 +305,6 @@ function ArtifactRow({ artifact }: any) {
         >
           <Text
             size="small"
-                        // @ts-expect-error
             weigth={500}
           >
             {artifact.name}

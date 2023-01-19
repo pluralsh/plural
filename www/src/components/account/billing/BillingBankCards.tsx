07cd9881bef35f50ff1c81e4a857fd4a071f5cda
David Hérault <dherault@gmail.com>
2023-01-19 17:07:00 +0100

    Add credit card

--- www/src/components/account/billing/BillingBankCards.tsx
+++ www/src/components/account/billing/BillingBankCards.tsx
@@ -1,13 +1,19 @@
 import { useCallback, useMemo, useState } from 'react'
 import { useQuery } from '@apollo/client'
-import { Button, Card } from '@pluralsh/design-system'
-import { Div, Spinner } from 'honorable'
-import { CardElement } from '@stripe/react-stripe-j'
+import { Button, Card, Input } from '@pluralsh/design-system'
+import { Div, Flex, Spinner } from 'honorable'
+import Cards from 'react-credit-cards'
+import 'react-credit-cards/es/styles-compiled.css'
 
 import { CARDS_QUERY } from './queries'
 
 function BillingBankCards() {
   const [edit, setEdit] = useState(false)
+  const [number, setNumber] = useState('')
+  const [name, setName] = useState('')
+  const [expiry, setExpiry] = useState('')
+  const [cvc, setCvc] = useState('')
+  const [focused, setFocused] = useState('')
   const { data, loading, error } = useQuery(CARDS_QUERY)
 
   const card = useMemo(() => data?.me?.cards?.edges?.[0]?.node ?? null, [data])
@@ -17,8 +23,63 @@ function BillingBankCards() {
   }, [])
 
   const renderEdit = useCallback(() => (
-    <Div />
-  ), [])
+    <Flex
+      align="start"
+      gap="medium"
+      width="100%"
+    >
+      <Cards
+        cvc={cvc}
+        expiry={expiry}
+        focused={focused}
+        name={name}
+        number={number}
+      />
+      <Flex
+        flexGrow={1}
+        direction="column"
+        gap="medium"
+      >
+        <Input
+          width="100%"
+          placeholder="Card number"
+          value={number}
+          onChange={event => setNumber(event.target.value)}
+          onFocus={() => setFocused('number')}
+        />
+        <Input
+          width="100%"
+          placeholder="Card holder name"
+          value={name}
+          onChange={event => setName(event.target.value)}
+          onFocus={() => setFocused('name')}
+        />
+        <Flex gap="medium">
+          <Input
+            flexGrow={1}
+            placeholder="Card expiry"
+            value={expiry}
+            onChange={event => setExpiry(event.target.value)}
+            onFocus={() => setFocused('expiry')}
+          />
+          <Input
+            flexGrow={1}
+            placeholder="CVC"
+            value={cvc}
+            onChange={event => setCvc(event.target.value)}
+            onFocus={() => setFocused('cvc')}
+          />
+        </Flex>
+        <Button alignSelf="flex-end">Add card</Button>
+      </Flex>
+    </Flex>
+  ), [
+    cvc,
+    expiry,
+    focused,
+    name,
+    number,
+  ])
 
   if (loading) {
     return (
@@ -43,6 +104,19 @@ function BillingBankCards() {
         color="text-xlight"
       >
         An error occured, please reload the page or contact support
+      </Card>
+    )
+  }
+
+  if (edit) {
+    return (
+      <Card
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        padding="medium"
+      >
+        {renderEdit()}
       </Card>
     )
   }
--- www/src/components/account/billing/BillingInvoices.tsx
+++ www/src/components/account/billing/BillingInvoices.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useMemo } from 'react'
+import { useMemo } from 'react'
 import { Card } from '@pluralsh/design-system'
 import { Div } from 'honorable'
 

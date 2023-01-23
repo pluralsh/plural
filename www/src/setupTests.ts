6fac93809ac3ae88fb2e05960be68404cb251d58
Sebastian Florek <sebastian@plural.sh>
2023-01-23 17:18:46 +0100

    Fix unit test

--- www/src/setupTests.ts
+++ www/src/setupTests.ts
@@ -1,8 +1,11 @@
 import { fetch } from 'cross-fetch'
 import 'regenerator-runtime/runtime'
+import styled from 'styled-components'
 
 // Polyfill fetch
 global.fetch = fetch
 
 // Mock the intercom calls
 global.Intercom = () => {}
+
+global.styled = styled

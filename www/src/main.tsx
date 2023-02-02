130b07419a93a865c07133f4322a887afe9a8200
David van der Spek <vanderspek.david@gmail.com>
2023-02-02 15:54:27 +0100

    add interface for cookiebot type
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- www/src/main.tsx
+++ www/src/main.tsx
@@ -6,6 +6,32 @@ import App from './App'
 
 import * as serviceWorker from './serviceWorkerRegistration'
 
+export interface Cookiebot {
+  consent: {
+    necessary: boolean;
+    preferences: boolean;
+    statistics: boolean;
+    marketing: boolean;
+    method: string | null;
+  };
+  consented: boolean;
+  declined: boolean;
+  hasResponse: boolean;
+  doNotTrack: boolean;
+  regulations: {
+    gdprApplies: boolean;
+    ccpaApplies: boolean;
+    lgpdApplies: boolean;
+  }
+  show(): void;
+  hide(): void;
+  getScript(url: string, async: boolean, callback: () => void): void;
+  withdraw(): void;
+  submitCustomConsent(optinPreferences: boolean, optinStatistics: boolean, optinMarketing: boolean): void;
+}
+
+declare const Cookiebot: Cookiebot
+
 posthog.init('phc_r0v4jbKz8Rr27mfqgO15AN5BMuuvnU8hCFedd6zpSDy',
   {
     api_host: 'https://posthog.plural.sh',

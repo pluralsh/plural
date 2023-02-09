e6fae6866667c4061f0fcabd2c017f5366ddae82
michaeljguarino <mguarino46@gmail.com>
2023-02-09 02:41:10 -0500

    don't show billing preview for enterprise plans

--- www/src/components/account/billing/BillingManagePlan.tsx
+++ www/src/components/account/billing/BillingManagePlan.tsx
@@ -1,13 +1,18 @@
 import { Div } from 'honorable'
+import { useContext } from 'react'
+
+import SubscriptionContext from '../../../contexts/SubscriptionContext'
 
 import BillingPreview from './BillingPreview'
 import BillingPricingCards from './BillingPricingCards'
 import BillingPricingTable from './BillingPricingTable'
 
 function BillingManagePlan() {
+  const { isEnterprisePlan } = useContext(SubscriptionContext)
+
   return (
     <>
-      <BillingPreview />
+      {!isEnterprisePlan && <BillingPreview />}
       <Div marginTop="xxlarge">
         <BillingPricingCards />
       </Div>

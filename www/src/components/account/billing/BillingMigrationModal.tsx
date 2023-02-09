cd4d13ad68984e4b5ab5af0afb8067530effb257
Sam Bolian <s.woodward90@gmail.com>
2023-02-09 16:53:50 -0600

    Update BillingMigrationModal.tsx

--- www/src/components/account/billing/BillingMigrationModal.tsx
+++ www/src/components/account/billing/BillingMigrationModal.tsx
@@ -33,7 +33,7 @@ function BillingMigrationModal() {
         We're excited to launch our new Pro tier pricing!  Our Pro tier is designed around enabling teams to collaborate in Plural without the need to become an Enterprise customer.
         <br />
         <br />
-        As a user with roles and service accounts enabled, you will now fall into the Pro tier which also includes unlimited app deployments and expert support with an SLA. We think you'll love it!
+        If you want to take advantage of roles, groups and other awesome features like VPN and expert support SLA, you can upgrade to Pro. We think you’ll love it! We think you'll love it!
         <br />
         <br />
         Read more about our new pricing plan on

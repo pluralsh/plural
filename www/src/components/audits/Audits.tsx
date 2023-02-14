0875ded0a2ce4f4becc64d7bdf1fb3d603d52352
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-02-14 13:50:29 +0100

    Update audit logs

--- www/src/components/audits/Audits.tsx
+++ www/src/components/audits/Audits.tsx
@@ -1,26 +1,79 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useMemo } from 'react'
 import { Box } from 'grommet'
 import { Link } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
-
-import { A, Div, Span } from 'honorable'
-
-import { PageTitle } from '@pluralsh/design-system'
+import { A, Div, Flex } from 'honorable'
+import { PageTitle, Table } from '@pluralsh/design-system'
+import { createColumnHelper } from '@tanstack/react-table'
+import isEmpty from 'lodash/isEmpty'
 
 import { extendConnection } from '../../utils/graphql'
-import { StandardScroller } from '../utils/SmoothScroller'
 import { LoopingLogo } from '../utils/AnimatedLogo'
-
-import { ReturnToBeginning } from '../utils/ReturnToBeginning'
-
-import { Table, TableData, TableRow } from '../utils/Table'
-
 import { AUDITS_Q } from '../account/queries'
-
 import { Date } from '../utils/Date'
 
 import { AuditUser } from './AuditUser'
 import { Location } from './Location'
+
+const FETCH_MARGIN = 30
+
+const COLUMN_HELPER = createColumnHelper<any>()
+
+const columns = [
+  COLUMN_HELPER.accessor(audit => audit, {
+    id: 'action',
+    cell: (audit: any) => {
+      const a = audit.getValue()
+      const { link, text } = resourceInfo(a)
+
+      return (
+        <>
+          {link && (
+            <A
+              inline
+              as={Link}
+              to={link}
+            >
+              {text}
+            </A>
+          )}
+          <Div
+            caption
+            color="text-xlight"
+          >
+            {a.action}
+          </Div>
+        </>
+      )
+    },
+    header: 'Package / Action',
+  }),
+  COLUMN_HELPER.accessor(audit => audit.actor, {
+    id: 'actor',
+    cell: (actor: any) => <AuditUser user={actor.getValue()} />,
+    header: 'Actor',
+  }),
+  COLUMN_HELPER.accessor(audit => audit.insertedAt, {
+    id: 'insertedAt',
+    cell: (insertedAt: any) => <Date date={insertedAt.getValue()} />,
+    header: 'Event time',
+  }),
+  COLUMN_HELPER.accessor(audit => audit, {
+    id: 'locationIp',
+    cell: (audit: any) => {
+      const { ip, country, city } = audit.getValue()
+
+      return (
+        <Location
+          ip={ip}
+          country={country}
+          city={city}
+        />
+      )
+    },
+    header: 'Location / IP',
+  }),
+]
 
 const versionLink = ({ chart, terraform }) => (chart ? `/charts/${chart.id}` : `/terraform/${terraform.id}`)
 
@@ -49,114 +102,51 @@ function resourceInfo({
   return { link: null, text: '' }
 }
 
-function Resource({ audit }: any) {
-  const { link, text } = resourceInfo(audit)
-
-  if (!link) return null
-
-  return (
-    <A
-      inline
-      as={Link}
-      to={link}
-    >
-      {text}
-    </A>
-  )
-}
-
-export function Placeholder() {
-  return (
-    <Box
-      flex={false}
-      height="50px"
-      pad="small"
-    />
-  )
-}
-
-function Audit({ audit, last }: any) {
-  return (
-    <TableRow last={last}>
-      <TableData>
-        <Resource audit={audit} />
-        <Div
-          caption
-          color="text-xlight"
-        >
-          {audit.action}
-        </Div>
-      </TableData>
-      <TableData>{audit.actor && <AuditUser user={audit.actor} />}</TableData>
-      <TableData><Date date={audit.insertedAt} /></TableData>
-      <TableData>
-        <Location
-          ip={audit.ip}
-          country={audit.country}
-          city={audit.city}
-        />
-      </TableData>
-    </TableRow>
-  )
-}
-
 export function Audits() {
-  const [listRef, setListRef] = useState<any>(null)
-  const [scrolled, setScrolled] = useState(false)
   const { data, loading, fetchMore } = useQuery(AUDITS_Q, { fetchPolicy: 'cache-and-network' })
+  const pageInfo = data?.audits?.pageInfo
+  const edges = data?.audits?.edges
+  const audits = useMemo(() => edges?.map(({ node }) => node) || [], [edges])
 
-  const returnToBeginning = useCallback(() => {
-    listRef.scrollToItem(0)
-  }, [listRef])
+  const fetchMoreOnBottomReached = useCallback((element?: HTMLDivElement | undefined) => {
+    if (!element) return
+
+    const { scrollHeight, scrollTop, clientHeight } = element
+
+        // Once scrolled within FETCH_MARGIN of the bottom of the table, fetch more data if there is any.
+    if (scrollHeight - scrollTop - clientHeight < FETCH_MARGIN && !loading && pageInfo.hasNextPage) {
+      fetchMore({
+        variables: { cursor: pageInfo.endCursor },
+        updateQuery: (prev, { fetchMoreResult: { audits } }) => extendConnection(prev, audits, 'audits'),
+      })
+    }
+  }, [fetchMore, loading, pageInfo])
 
   if (!data) {
     return (
-      <LoopingLogo />
+      <Flex
+        grow={1}
+        justify="center"
+      >
+        <LoopingLogo />
+      </Flex>
     )
   }
-
-  const { edges, pageInfo } = data.audits
 
   return (
     <Box
       fill
     >
       <PageTitle heading="Audit logs" />
-      {edges.length
+      {!isEmpty(audits)
         ? (
           <Table
-            headers={['Package / Action', 'Actor', 'Event time', 'Location / IP']}
-            sizes={['30%', '25%', '25%', '20%']}
-            width="100%"
-            height="100%"
-            background="fill-one"
-            border="1px solid border"
-          >
-            <Box fill>
-              {scrolled && <ReturnToBeginning beginning={returnToBeginning} />}
-              <StandardScroller
-                listRef={listRef}
-                setListRef={setListRef}
-                hasNextPage={pageInfo.hasNextPage}
-                items={edges}
-                loading={loading}
-                handleScroll={setScrolled}
-                placeholder={Placeholder}
-                mapper={({ node: audit }, { next }) => (
-                  <Audit
-                    last={!next.node}
-                    key={audit.id}
-                    audit={audit}
-                  />
-                )}
-                loadNextPage={() => pageInfo.hasNextPage && fetchMore({
-                  variables: { cursor: pageInfo.endCursor },
-                  updateQuery: (prev, { fetchMoreResult: { audits } }) => extendConnection(prev, audits, 'audits'),
-                })}
-              />
-            </Box>
-          </Table>
-        ) : <Span>You do not have any audit logs yet.</Span>}
+            data={audits}
+            columns={columns}
+            onScrollCapture={e => fetchMoreOnBottomReached(e?.target)}
+            maxHeight="calc(100vh - 244px)"
+          />
+        ) : <>You do not have any audit logs yet.</>}
     </Box>
   )
 }

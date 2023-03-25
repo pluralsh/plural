3d66a2b2b03dd30835c17e86c5a14fdea0b86a31
Klink <85062+dogmar@users.noreply.github.com>
2023-03-24 17:30:55 -0700

    Put invoices into table

--- www/src/components/account/billing/BillingInvoices.tsx
+++ www/src/components/account/billing/BillingInvoices.tsx
@@ -1,41 +1,74 @@
 import { useMemo } from 'react'
-import { Button, Card, DocumentIcon } from '@pluralsh/design-system'
+import {
+  Button,
+  Card,
+  DocumentIcon,
+  EmptyState,
+  Table,
+} from '@pluralsh/design-system'
 import { Div, Flex } from 'honorable'
 import moment from 'moment'
+import { createColumnHelper } from '@tanstack/react-table'
 
-import { type InvoiceFragment, useInvoicesQuery } from '../../../generated/graphql'
+import capitalize from 'lodash/capitalize'
+import isEmpty from 'lodash/isEmpty'
+
+import { Invoice, useInvoicesQuery } from '../../../generated/graphql'
 
 import BillingError from './BillingError'
 import BillingLoading from './BillingLoading'
 
-function InvoiceLine({ invoice, i }: { invoice: InvoiceFragment; i: number }) {
-  return (
-    <Div
-      key={invoice.number}
-      backgroundColor={i % 2 ? 'fill-one' : 'fill-two'}
-      display="grid"
-      gridTemplateColumns="repeat(3, 1fr)"
-      paddingVertical="xsmall"
-      paddingHorizontal="medium"
-    >
-      <Flex align="center">{moment(invoice.createdAt).format('MM/DD/YY')}</Flex>
-      <Flex align="center">${invoice.amountPaid / 100}</Flex>
-      <Flex justify="flex-end">
+const columnHelper = createColumnHelper<Invoice>()
+
+const columns = [
+  columnHelper.accessor(row => row.createdAt, {
+    id: 'date',
+    enableSorting: true,
+    cell: info => moment(info.getValue()).format('MM/DD/YY'),
+    header: () => <>Date</>,
+  }),
+  columnHelper.accessor(row => row.amountDue, {
+    id: 'amountDue',
+    enableSorting: true,
+    cell: info => `$${(info.getValue() / 100).toFixed(2)}`,
+    header: () => <>Amount due</>,
+  }),
+  columnHelper.accessor(row => row.amountPaid, {
+    id: 'amountPaid',
+    enableSorting: true,
+    cell: info => `$${(info.getValue() / 100).toFixed(2)}`,
+    header: () => <>Amount paid</>,
+  }),
+  columnHelper.accessor(row => row.status, {
+    id: 'status',
+    enableSorting: true,
+    cell: info => capitalize(info.getValue() || ''),
+    header: () => <>Status</>,
+  }),
+  columnHelper.accessor(row => row, {
+    id: 'viewInvoice',
+    cell: info => (
+      <Flex
+        width="100%"
+        justifyContent="flex-end"
+      >
         <Button
           as="a"
-          href={invoice.hostedInvoiceUrl}
+          href={info.getValue()}
           target="_blank"
           rel="noopener noreferer"
-          tertiary
+          secondary
+          small
           padding={0}
           startIcon={<DocumentIcon />}
         >
-          View invoice
+          View/pay invoice
         </Button>
       </Flex>
-    </Div>
-  )
-}
+    ),
+    header: () => <Div />,
+  }),
+]
 
 function BillingInvoices() {
   const { data, loading, error } = useInvoicesQuery()
@@ -43,49 +76,32 @@ function BillingInvoices() {
   const invoices = useMemo(() => data?.invoices?.edges?.map(e => e?.node),
     [data])
 
-  let content
-
   if (error) {
-    content = <BillingError />
+    return (
+      <Card>
+        <BillingError />
+      </Card>
+    )
   }
-  else if (loading) {
-    content = <BillingLoading />
-  }
-  else {
-    content = (
-      <>
-        <Div
-          display="grid"
-          gridTemplateColumns="repeat(3, 1fr)"
-          padding="medium"
-        >
-          <Div
-            body2
-            fontWeight={600}
-          >
-            Date
-          </Div>
-          <Div
-            body2
-            fontWeight={600}
-          >
-            Amount
-          </Div>
-        </Div>
-        <Div>
-          {invoices?.map((invoice, i) => invoice && (
-            <InvoiceLine
-              key={invoice.number}
-              i={i}
-              invoice={invoice}
-            />
-          ))}
-        </Div>
-      </>
+  if (loading) {
+    return (
+      <Card>
+        <BillingLoading />
+      </Card>
     )
   }
 
-  return <Card>{content}</Card>
+  if (isEmpty(invoices)) {
+    return <Card><EmptyState message="No invoices created yet" /></Card>
+  }
+
+  return (
+    <Table
+      loose
+      data={invoices || []}
+      columns={columns}
+    />
+  )
 }
 
 export default BillingInvoices

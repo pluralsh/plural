5955fae27ee7e7813677b75d82e0e7ce22e6a8a2
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-03-27 12:14:10 +0200

    Update legacy user message

--- www/src/components/account/billing/BillingLegacyUserMessage.tsx
+++ www/src/components/account/billing/BillingLegacyUserMessage.tsx
@@ -1,37 +1,51 @@
 import { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { A, P } from 'honorable'
-
 import moment from 'moment'
+import styled from 'styled-components'
+import { ErrorIcon, IconFrame } from '@pluralsh/design-system'
+import { Link } from 'react-router-dom'
 
 import SubscriptionContext from '../../../contexts/SubscriptionContext'
 
+const Wrap = styled.div({ display: 'flex', alignItems: 'center' })
+
+const Message = styled.p(({ theme }) => ({
+  ...theme.partials.text.overline,
+  color: theme.colors['text-xlight'],
+}))
+
+const MessageLink = styled.a(({ theme }) => ({ ...theme.partials.text.inlineLink }))
+
 function BillingLegacyUserMessage() {
   const {
-    isProPlan, isEnterprisePlan, isGrandfathered, account,
+    isPaidPlan, isGrandfathered, isGrandfathetingExpired, account,
   } = useContext(SubscriptionContext)
 
-  const isLegacy = !(isProPlan || isEnterprisePlan) && account?.grandfatheredUntil
-  const expired = !isGrandfathered
+  if (isPaidPlan || !(isGrandfathered || isGrandfathetingExpired)) return null
 
-  if (!isLegacy) return null
+  const message = isGrandfathetingExpired
+    ? 'Legacy user access expired. '
+    : `Legacy user access until ${moment(account?.grandfatheredUntil).format('MMM DD, YYYY')}. `
 
   return (
-    <P
-      overline
-      color="text-xlight"
-    >
-      Legacy user access {expired ? 'expired' : `until ${moment(account?.grandfatheredUntil).format('MMM DD, YYYY')}`}
-      {' '}
-      <A
-        inline
-        as={Link}
-        to="/account/billing"
-      >
-        upgrade now
-      </A>
-    </P>
+    <Wrap>
+      {isGrandfathetingExpired && (
+        <IconFrame
+          icon={<ErrorIcon color="icon-error" />}
+          textValue={message}
+        />
+      )}
+      <Message>
+        {message}
+        <MessageLink
+          as={Link}
+          to="/account/billing"
+        >
+          upgrade now
+        </MessageLink>
+      </Message>
+    </Wrap>
   )
 }
 
 export default BillingLegacyUserMessage
+

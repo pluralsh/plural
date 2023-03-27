6f3c902c4efb2312bd855724da7672263bb04fa6
Klink <85062+dogmar@users.noreply.github.com>
2023-03-27 14:47:43 -0700

    Address form almost working

--- www/src/components/account/billing/BillingUpgradeToProfessionalModal.tsx
+++ www/src/components/account/billing/BillingUpgradeToProfessionalModal.tsx
@@ -1,25 +1,6 @@
-import {
-  useCallback,
-  useContext,
-  useEffect,
-  useState,
-} from 'react'
-import { Div, Flex } from 'honorable'
-import { Button, LoopingLogo, Modal } from '@pluralsh/design-system'
+import { useCallback, useState } from 'react'
+import { Modal } from '@pluralsh/design-system'
 
-import PlatformPlansContext from '../../../contexts/PlatformPlansContext'
-
-import {
-  Maybe,
-  PaymentIntentFragment,
-  namedOperations,
-  useUpgradeToProfessionalPlanMutation,
-} from '../../../generated/graphql'
-
-import { GqlError } from '../../utils/Alert'
-
-import BillingPreview from './BillingPreview'
-import { StripeElements } from './StripeElements'
 import PaymentForm from './PaymentForm'
 
 type BillingUpgradeToProfessionalModalPropsType = {
@@ -31,104 +12,9 @@ function BillingUpgradeToProfessionalModal({
   open,
   onClose: onCloseProp,
 }: BillingUpgradeToProfessionalModalPropsType) {
-  const { proPlatformPlan, proYearlyPlatformPlan }
-    = useContext(PlatformPlansContext)
-  const [applyYearlyDiscount, setApplyYearlyDiscount] = useState(false)
-  const [paymentIntent, setPaymentIntent] = useState<
-    PaymentIntentFragment | null | undefined
-  >(undefined)
-
-  const planId = applyYearlyDiscount
-    ? proYearlyPlatformPlan.id
-    : proPlatformPlan.id
-
-  console.log('planId', planId)
-
-  const [upgradeMutation, { loading, error }]
-    = useUpgradeToProfessionalPlanMutation({
-      refetchQueries: [namedOperations.Query.Subscription],
-      onCompleted: ret => {
-        const intent
-          = ret.createPlatformSubscription?.latestInvoice?.paymentIntent
-
-        console.log('paymentIntent', intent)
-        setPaymentIntent(intent)
-      },
-      onError: error => {
-        console.log('mutation error', error.message)
-      },
-    })
-
-  console.log('loading', loading)
-
-  useEffect(() => {
-    console.log('run upgradeMutation', upgradeMutation)
-    upgradeMutation({ variables: { planId } })
-  }, [planId, upgradeMutation])
-
   const onClose = useCallback(() => {
     onCloseProp()
   }, [onCloseProp])
-
-  const insideContent = paymentIntent?.clientSecret ? (
-    <StripeElements
-      options={{ clientSecret: paymentIntent.clientSecret ?? undefined }}
-    >
-      <PaymentForm />
-    </StripeElements>
-  ) : (
-    <Div
-      width="100%"
-      overflow="hidden"
-    >
-      {error ? <GqlError error={error} /> : <LoopingLogo />}
-    </Div>
-  )
-
-  const content = (
-    <>
-      <BillingPreview
-        noCard
-        discountPreview
-        yearly={applyYearlyDiscount}
-        onChange={setApplyYearlyDiscount}
-      />
-      <Div
-        fontWeight="bold"
-        marginTop="large"
-        marginBottom="medium"
-      >
-        Billing information
-      </Div>
-      <Flex
-        flexDirection="column"
-        gap="xlarge"
-      >
-        {insideContent}
-      </Flex>
-      <Flex
-        justify="flex-end"
-        marginTop="xxlarge"
-        gap="small"
-      >
-        <Button
-          secondary
-          onClick={() => {
-            onClose()
-          }}
-        >
-          Cancel
-        </Button>
-        {/* <Button
-          loading={loading}
-          disabled={!card}
-          onClick={onClickUpgrade}
-        >
-          Upgrade
-        </Button> */}
-      </Flex>
-    </>
-  )
 
   return (
     <Modal
@@ -137,7 +23,7 @@ function BillingUpgradeToProfessionalModal({
       header="Upgrade to professional"
       minWidth={512 + 128}
     >
-      {content}
+      <PaymentForm />
     </Modal>
   )
 }
--- www/src/components/account/billing/PaymentForm.tsx
+++ www/src/components/account/billing/PaymentForm.tsx
@@ -1,50 +1,216 @@
-import { Button, LoadingSpinner } from '@pluralsh/design-system'
-import { PaymentElement, useElements, useStripe } from '@stripe/react-stripe-js'
+import { Button } from '@pluralsh/design-system'
+import {
+  AddressElement,
+  CardElement,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from '@stripe/react-stripe-js'
 import { StripePaymentElementOptions } from '@stripe/stripe-js'
-import { useEffect, useState } from 'react'
+import { Div, Flex } from 'honorable'
+import {
+  PropsWithChildren,
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from 'react'
+import { type ImmerReducer, useImmerReducer } from 'use-immer'
+
+import PlatformPlansContext from '../../../contexts/PlatformPlansContext'
+import SubscriptionContext from '../../../contexts/SubscriptionContext'
+import { PaymentIntentFragment, namedOperations, useUpgradeToProfessionalPlanMutation } from '../../../generated/graphql'
 
 import { host } from '../../../helpers/hostname'
+import { GqlError } from '../../utils/Alert'
 
 import BillingError from './BillingError'
+import BillingPreview from './BillingPreview'
+
+enum PaymentFormState {
+  CollectAddress = 'ADDRESS',
+  CollectPayment = 'PAYMENT',
+}
+
+type PaymentFormContextState = {
+  formState: PaymentFormState
+  clientSecret?: string | undefined | null
+  paymentIntent?: PaymentIntentFragment | undefined | null
+}
+
+type PaymentFormContextVal = PaymentFormContextState & {
+  setClientSecret: (
+    clientSecret?: PaymentFormContextState['clientSecret']
+  ) => void
+  setPaymentIntent: (
+    paymentIntent?: PaymentFormContextState['paymentIntent']
+  ) => void
+  setFormState: (state: PaymentFormState) => void
+}
+
+const defaultState: PaymentFormContextState = {
+  formState: PaymentFormState.CollectAddress,
+  clientSecret: undefined,
+  paymentIntent: undefined,
+}
+
+const PaymentFormContext = createContext<PaymentFormContextVal | null>(null)
+
+export const usePaymentForm = () => {
+  const context = useContext(PaymentFormContext)
+
+  if (!context) {
+    throw Error('usePaymentForm() must be used within <PaymentFormProvider />')
+  }
+
+  return context
+}
+
+const reducer: ImmerReducer<
+  PaymentFormContextState,
+  | {
+      type: 'setClientSecret'
+      payload: PaymentFormContextState['clientSecret']
+    }
+  | {
+      type: 'setPaymentIntent'
+      payload: PaymentFormContextState['paymentIntent']
+    }
+  | { type: 'setFormState'; payload: PaymentFormState }
+> = (draft, action) => {
+  switch (action.type) {
+  case 'setClientSecret':
+    draft.clientSecret = action.payload
+
+    return draft
+    break
+  case 'setFormState':
+    draft.formState = action.payload
+
+    return draft
+  case 'setPaymentIntent':
+    draft.paymentIntent = action.payload
+
+    return draft
+
+  default:
+    console.error('Incorrect action type sent to reducer')
+
+    return draft
+  }
+}
+const paymentElementOptions: StripePaymentElementOptions = {
+  layout: 'tabs',
+}
+
+function PaymentFormProvider({
+  children,
+}: PropsWithChildren) {
+  const [contextState, dispatch] = useImmerReducer(reducer, defaultState)
+
+  const contextVal = useMemo(() => ({
+    ...contextState,
+    setClientSecret: (clientSecret: PaymentFormContextState['clientSecret']) => {
+      dispatch({ type: 'setClientSecret', payload: clientSecret })
+    },
+    setFormState: (state: PaymentFormContextState['formState']) => {
+      dispatch({ type: 'setFormState', payload: state })
+    },
+    setPaymentIntent: (paymentIntent: PaymentFormContextState['paymentIntent']) => {
+      dispatch({ type: 'setPaymentIntent', payload: paymentIntent })
+    },
+  }),
+  [dispatch, contextState])
+
+  return (
+    <PaymentFormContext.Provider value={contextVal}>
+      {children}
+    </PaymentFormContext.Provider>
+  )
+}
+
+function PaymentFormInner() {
+  const { proPlatformPlan, proYearlyPlatformPlan }
+    = useContext(PlatformPlansContext)
+  const { formState, setClientSecret, setPaymentIntent } = usePaymentForm()
+  const [applyYearlyDiscount, setApplyYearlyDiscount] = useState(false)
+  const planId = applyYearlyDiscount
+    ? proYearlyPlatformPlan.id
+    : proPlatformPlan.id
+  // const [loading, setLoading] = useState(false)
+
+  if (formState === PaymentFormState.CollectAddress) {
+    console.log('thing')
+  }
+  const [upgradeMutation, { loading, error }]
+    = useUpgradeToProfessionalPlanMutation({
+      refetchQueries: [namedOperations.Query.Subscription],
+      onCompleted: ret => {
+        const intent
+          = ret.createPlatformSubscription?.latestInvoice?.paymentIntent
+
+        console.log('paymentIntent', intent)
+        setPaymentIntent(intent)
+        setClientSecret(intent?.clientSecret)
+      },
+      onError: error => {
+        console.log('mutation error', error.message)
+      },
+    })
+
+  return (
+    <>
+      {formState === PaymentFormState.CollectAddress && (
+        <>
+          <BillingPreview
+            noCard
+            discountPreview
+            yearly={applyYearlyDiscount}
+            onChange={setApplyYearlyDiscount}
+          />
+          <Div
+            fontWeight="bold"
+            marginTop="large"
+            marginBottom="medium"
+          >
+            Billing information
+          </Div>
+          <Flex
+            flexDirection="column"
+            gap="xlarge"
+          >
+            <AddressForm
+              loading={loading}
+              error={error?.message}
+              onSubmit={address => {
+                console.log('address submit')
+                upgradeMutation({
+                  variables: { planId, billingAddress: address },
+                })
+              }}
+            />
+          </Flex>
+        </>
+      )}
+      {/* {formState === PaymentFormState.CollectPayment} && <Payment /> */}
+    </>
+  )
+}
 
 export default function PaymentForm() {
+  return (
+    <PaymentFormProvider>
+      <PaymentFormInner />
+    </PaymentFormProvider>
+  )
+}
+
+function Payment() {
+  const [message, setMessage] = useState<string | null | undefined>()
+  const [isLoading, setIsLoading] = useState(false)
   const stripe = useStripe()
   const elements = useElements()
-
-  //   const [_email, setEmail] = useState('')
-  const [message, setMessage] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
-
-  // This is actually handled in BillingManagePlan > ConfirmPayment
-
-  //   useEffect(() => {
-  //     if (!stripe) {
-  //       return
-  //     }
-
-  //     const clientSecret = new URLSearchParams(window.location.search).get('payment_intent_client_secret')
-
-  //     if (!clientSecret) {
-  //       return
-  //     }
-
-  //     stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
-  //       switch (paymentIntent?.status) {
-  //       case 'succeeded':
-  //         setMessage('Payment succeeded!')
-  //         break
-  //       case 'processing':
-  //         setMessage('Your payment is processing.')
-  //         break
-  //       case 'requires_payment_method':
-  //         setMessage('Your payment was not successful, please try again.')
-  //         break
-  //       default:
-  //         setMessage('Something went wrong.')
-  //         break
-  //       }
-  //     })
-  //   }, [stripe])
 
   const handleSubmit = async e => {
     e.preventDefault()
@@ -80,28 +246,19 @@ export default function PaymentForm() {
     setIsLoading(false)
   }
 
-  if (!stripe || !elements) {
-    return <LoadingSpinner />
-    // TODO: Better loading state
-  }
-
-  const paymentElementOptions: StripePaymentElementOptions = {
-    layout: 'tabs',
-  }
-
   return (
     <form
       id="payment-form"
       onSubmit={handleSubmit}
     >
-      {/* <LinkAuthenticationElement
-        id="link-authentication-element"
-        onChange={e => setEmail(e.target.value)}
-      /> */}
       <PaymentElement
         id="payment-element"
         options={paymentElementOptions}
       />
+      {/* Show any error or success messages */}
+
+      {message && <BillingError>{message}</BillingError>}
+
       <Button
         type="submit"
         loading={isLoading}
@@ -110,8 +267,140 @@ export default function PaymentForm() {
       >
         Upgrade now
       </Button>
-      {/* Show any error or success messages */}
-      {message && <BillingError>{message}</BillingError>}
+    </form>
+  )
+}
+
+function AddressForm({
+  loading: loadingProp = false,
+  error: errorProp = '',
+  onSubmit,
+}: {
+  loading?: boolean
+  error?: string
+  onSubmit: (addr: {
+    city: string
+    country: string
+    line1: string
+    line2: string
+    name: string
+    state: string
+    zip: string
+  }) => void
+}) {
+  const [addressComplete, setAddressComplete] = useState(false)
+
+  const [validating, setValidating] = useState(loadingProp)
+  const [formError, setFormError] = useState(errorProp)
+  const loading = loadingProp || validating
+  const stripe = useStripe()
+  const elements = useElements()
+  const validateForm = useCallback(async () => {
+    if (!elements) {
+      return
+    }
+    const addressElt = elements.getElement(AddressElement)
+
+    await (addressElt as any)?.getValue()
+  }, [elements])
+  const { billingAddress } = useContext(SubscriptionContext)
+  const disableSubmit = !stripe || !elements || !addressComplete || loading
+
+  const defaultAddress = useMemo(() => ({
+    name: billingAddress?.name ?? '',
+    address: {
+      line1: billingAddress?.line1 ?? '',
+      line2: billingAddress?.line2 ?? '',
+      city: billingAddress?.city ?? '',
+      state: billingAddress?.state ?? '',
+      country: billingAddress?.country ?? '',
+      postal_code: billingAddress?.zip ?? '',
+    },
+  }),
+  [billingAddress])
+
+  const handleSubmit = useCallback(async event => {
+    event.preventDefault()
+
+    if (!(stripe && elements)) return
+
+    setValidating(true)
+
+    const addressElt = elements.getElement(AddressElement)
+    const cardElt = elements.getElement(CardElement)
+
+    if (!(addressElt && cardElt)) {
+      return
+    }
+
+    const addressVal = await (addressElt as any)?.getValue()
+
+    if (!addressVal.complete) {
+      setValidating(false)
+      setAddressComplete(false)
+      setFormError('Your billing address is incomplete.')
+
+      return
+    }
+
+    const { address, name } = addressVal.value
+
+    try {
+      onSubmit({
+        name: name || '',
+        line1: address.line1 || '',
+        line2: address.line2 || '',
+        zip: address.zip || '',
+        state: address.state || '',
+        city: address.city || '',
+        country: address.country || '',
+      })
+    }
+    catch (error) {
+      setFormError((error as Error)?.message)
+      setValidating(false)
+
+      return
+    }
+
+    setValidating(false)
+  },
+  [elements, onSubmit, stripe])
+
+  const error = formError || errorProp
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Flex
+        flexDirection="column"
+        gap="xlarge"
+      >
+        <AddressElement
+          options={{
+            mode: 'billing',
+            defaultValues: defaultAddress,
+          }}
+          onChange={event => {
+            setAddressComplete(event.complete)
+            setFormError('')
+          }}
+        />
+        {error && <BillingError>{error}</BillingError>}
+
+        <Flex justify="end"><Button
+          primary
+          disabled={disableSubmit}
+          loading={loading}
+          onClick={() => {
+            validateForm()
+          }}
+        >
+          Continue to payment
+                            </Button>
+        </Flex>
+
+      </Flex>
+
     </form>
   )
 }

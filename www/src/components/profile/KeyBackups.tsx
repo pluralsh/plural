ed45be6c4e4adc007a9996b6c200b2ac123529d6
Klink <85062+dogmar@users.noreply.github.com>
2023-03-31 11:04:52 -0700

    Finish keyBackup table

--- www/src/components/profile/KeyBackups.tsx
+++ www/src/components/profile/KeyBackups.tsx
@@ -1,31 +1,56 @@
-import { Box } from 'grommet'
 import {
   A,
+  Button,
   Div,
   Flex,
   P,
 } from 'honorable'
 import moment from 'moment'
-import { useMemo, useState } from 'react'
-import { ColumnDef, createColumnHelper } from '@tanstack/react-table'
+import { useCallback, useState } from 'react'
+import { createColumnHelper } from '@tanstack/react-table'
 
 import {
+  Codeline,
   DownloadIcon,
   EmptyState,
   IconFrame,
   Modal,
   PageTitle,
+  Table,
+  Tooltip,
 } from '@pluralsh/design-system'
 
 import isEmpty from 'lodash/isEmpty'
 
+import styled from 'styled-components'
+
 import { Confirm } from '../utils/Confirm'
 import { DeleteIconButton } from '../utils/IconButtons'
 import LoadingIndicator from '../utils/LoadingIndicator'
-import { KeyBackupFragment, useDeleteKeyBackupMutation, useKeyBackupsQuery } from '../../generated/graphql'
-import { Table } from '../utils/Table'
+import {
+  KeyBackupFragment,
+  KeyBackupsDocument,
+  useCreateKeyBackupMutation,
+  useDeleteKeyBackupMutation,
+  useKeyBackupsQuery,
+} from '../../generated/graphql'
+import { GqlError } from '../utils/Alert'
+import { appendConnection, removeConnection, updateCache } from '../../utils/graphql'
 
 const columnHelper = createColumnHelper<KeyBackupFragment>()
+
+const BareUl = styled.ul(_ => ({
+  margin: 0,
+  padding: 0,
+  maxWidth: '400px',
+}))
+
+const BareLi = styled.li(_ => ({
+  margin: 0,
+  padding: 0,
+  listStyle: 'none',
+  maxWidth: 'max-content',
+}))
 
 const columns = [
   columnHelper.accessor(key => key.name, {
@@ -35,74 +60,131 @@ const columns = [
   }),
   columnHelper.accessor(key => key.repositories, {
     id: 'repositories',
-    cell: info => <>{info.getValue()}</>,
+    cell: info => (
+      <BareUl>
+        {info.getValue()?.map(repo => (
+          <Tooltip
+            placement="top-start"
+            label={repo}
+          >
+            <BareLi>{repo}</BareLi>
+          </Tooltip>
+        ))}
+      </BareUl>
+    ),
     header: 'Respositories',
+    meta: { truncate: true },
   }),
   columnHelper.accessor(key => key.digest, {
     id: 'digest',
-    cell: info => <>{info.getValue()}</>,
+    cell: info => (
+      <Tooltip
+        placement="top-start"
+        label={info.getValue()}
+      >
+        <Div maxWidth="max-content">{info.getValue()}</Div>
+      </Tooltip>
+    ),
     header: 'Digest',
+    meta: { truncate: true },
   }),
   columnHelper.accessor(key => key.insertedAt, {
     id: 'createdOn',
     cell: info => {
       const date = moment(info.getValue())
       const formattedDate = date.format('MM/DD/YY')
-      const formattedTime = date.format('h:mm:ss a')
+      const formattedTime = date.format('h:mma')
 
       return (
         <Flex direction="column">
-          <Div>{formattedDate}</Div>
-          <Div>{formattedTime}</Div>
+          <P body2>{formattedDate}</P>
+          <P
+            caption
+            color="text-light"
+          >
+            {formattedTime}
+          </P>
         </Flex>
       )
     },
     header: 'Created on',
   }),
-  columnHelper.accessor(key => key, {
+  columnHelper.accessor(() => null, {
     id: 'actions',
-    cell: info => <>{info.getValue()}</>,
-    // header: 'Name',
+    cell: ({ row: { original } }) => (
+      <Flex
+        flexDirection="row"
+        gap="xxsmall"
+      >
+        <LocalSyncButton />
+        <DeleteKeyBackup name={original.name} />
+      </Flex>
+    ),
+    header: '',
   }),
 ]
 
-function DeleteKeyBackup({ name, refetch }) {
+function DeleteKeyBackup({ name }) {
+  // Disable until deleteKeyBackup mutation is working
+  // return null
+
   const [confirm, setConfirm] = useState(false)
-  const [mutation, { loading }] = useDeleteKeyBackupMutation({
+  const [mutation, { loading, error, reset }] = useDeleteKeyBackupMutation({
     variables: { name },
     onCompleted: () => {
       setConfirm(false)
-      refetch()
     },
+    update: (cache, { data }) => updateCache(cache, {
+      query: KeyBackupsDocument,
+      variables: {},
+      update: prev => removeConnection(prev, data?.deleteKeyBackup, 'keyBackups'),
+    }),
   })
+  const open = useCallback(() => {
+    setConfirm(true)
+  }, [])
+  const close = useCallback(() => {
+    reset()
+    setConfirm(false)
+  }, [reset])
 
   return (
     <Div onClick={e => e.stopPropagation()}>
       <DeleteIconButton
-        onClick={() => setConfirm(true)}
+        onClick={open}
         textValue="Delete key backup"
         tooltip
       />
-      {confirm && (
-        <div>
-          <Confirm
-            close={() => setConfirm(false)}
-            destructive
-            label="Confirm delete"
-            loading={loading}
-            open={confirm}
-            submit={() => mutation()}
-            title="Delete encryption key backup"
-            text={`Are you sure you want to delete the encryption key: ${name}?`}
-          />
-        </div>
-      )}
+      <div>
+        <Confirm
+          close={close}
+          destructive
+          label="Confirm delete"
+          loading={loading}
+          open={confirm}
+          submit={() => mutation()}
+          title="Delete encryption key backup"
+          text={(
+            <Flex
+              direction="column"
+              gap="medium"
+            >
+              <P>
+                Are you sure you want to delete the encryption key backup:{' '}
+                {name}?
+              </P>
+              {error && <GqlError error={error} />}
+            </Flex>
+          )}
+        />
+      </div>
     </Div>
   )
 }
 
 function LocalSyncButton() {
   const [isOpen, setIsOpen] = useState(false)
+  const close = useCallback(() => setIsOpen(false), [])
 
   return (
     <>
@@ -117,38 +199,62 @@ function LocalSyncButton() {
       />
       <Modal
         open={isOpen}
-        onClose={setIsOpen(false)}
+        onClose={close}
+        header="Sync encryption keys locally"
+        size="large"
+        portal
       >
-        Sync messaging
+        <Flex
+          direction="column"
+          gap="medium"
+        >
+          <P body1>
+            To sync your encryption keys locally, run this command in the Plural
+            CLI on your local machine:
+          </P>
+          <Codeline>plural crypto backups restore</Codeline>
+        </Flex>
       </Modal>
     </>
   )
 }
 
-const ColActions = refetch => columnHelper.accessor(() => null, {
-  id: 'actions',
-  cell: ({ row: { original } }) => (
-    <Flex
-      flexDirection="row"
-      gap="xxsmall"
-    >
-      <LocalSyncButton />
-      <DeleteKeyBackup
-        name={original.name}
-        refetch={refetch}
-      />
-    </Flex>
-  ),
-  header: '',
-})
+function CreateKeyButton() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <>
+      <Button
+        secondary
+        onClick={() => {
+          setIsOpen(true)
+        }}
+      >
+        Create key backup
+      </Button>
+      {isOpen && (
+        <Modal
+          open={isOpen}
+          onClose={setIsOpen(false)}
+        >
+          Sync messaging
+        </Modal>
+      )}
+    </>
+  )
+}
 
 type Edge<T> = { node?: T } | null | undefined
+type Connection<T> = {
+  edges?: Edge<T | null | undefined>[] | null | undefined
+}
 
-function mapExistingEdgeNodes<T, R>(edges: Edge<T | null | undefined>[] | null | undefined,
+export function mapExistingConnectionNodes<T, R>(connection: Connection<T> | null | undefined,
   mapper: (node: T) => R) {
-  if (!edges) {
+  if (!connection?.edges) {
     return undefined
   }
+  const { edges } = connection
 
   return edges.reduce((prev, edge) => {
     if (edge?.node) {
@@ -159,65 +265,114 @@ function mapExistingEdgeNodes<T, R>(edges: Edge<T | null | undefined>[] | null |
   }, [] as R[])
 }
 
+function CreateDummyKeyButton() {
+  const [keyi, setkeyi] = useState(0)
+
+  const [createMutation, { loading: createLoading }]
+    = useCreateKeyBackupMutation({
+      variables: {
+        attributes: {
+          name: `name${String(Math.trunc(keyi)).padStart(5, '0')}`,
+          repositories: [
+            'git@github.com:/pluralsh/plural.git',
+            'git@github.com:/pluralsh/some-long-name-of-something-or-other/plural.git',
+          ],
+          key: `value${String(Math.trunc(keyi)).padStart(5, '0')}`,
+        },
+      },
+      onCompleted: data => {
+        console.log('completed', data)
+      },
+      onError: error => {
+        console.log('error', error)
+      },
+      update: (cache, { data }) => updateCache(cache, {
+        query: KeyBackupsDocument,
+        variables: {},
+        update: prev => appendConnection(prev, data?.createKeyBackup, 'keyBackups'),
+      }),
+    })
+
+  return (
+    <Button
+      secondary
+      loading={createLoading}
+      onClick={() => {
+        createMutation()
+        setkeyi(keyi + 1)
+      }}
+    >
+      Debug create
+    </Button>
+  )
+}
+
 export function KeyBackups() {
-  const {
-    data, loading, fetchMore, refetch,
-  } = useKeyBackupsQuery({})
+  const { data, error } = useKeyBackupsQuery({
+    pollInterval: 10000,
+  })
 
-  const keyBackups = mapExistingEdgeNodes(data?.keyBackups?.edges, edge => edge)
+  const keyBackups = mapExistingConnectionNodes(data?.keyBackups,
+    edge => edge)?.sort((a, b) => (a.name === b.name ? 0 : a.name < b.name ? -1 : 1))
 
-  // Memoize columns to prevent rerendering entire table
-  const memoizedCols: ColumnDef<KeyBackupFragment, any>[] = useMemo(() => [...columns, ColActions(refetch)],
-    [refetch])
+  console.log('k', keyBackups)
+
+  if (error) return <GqlError error={error} />
 
   if (!keyBackups) return <LoadingIndicator />
 
   return (
-    <Box fill>
+    <Flex
+      direction="column"
+      maxHeight="100%"
+      overflow="hidden"
+    >
       <PageTitle
-        heading="Encryption keys"
+        heading={(
+          <>
+            <div>Encryption keys</div>
+            <P
+              body2
+              maxWidth={450}
+              color="text-light"
+            >
+              Secure cloud backups of the machine-local AES encryption keys for
+              your installation repositories.{' '}
+              <A
+                href="https://docs.plural.sh/getting-started/manage-git-repositories/workspace-encryption"
+                target="_blank"
+                inline
+              >
+                Learn more
+              </A>
+            </P>
+          </>
+        )}
         justifyContent="flex-start"
       >
-        <P
-          marginBottom="medium"
-          maxWidth={400}
+        <Flex
+          // height="100%"
+          flexGrow={1}
+          flexShrink={1}
+          alignItems="flex-start"
+          justifyContent="flex-end"
         >
-          Secure cloud backups of the machine-local AES encryption keys for your
-          installation repositories.{' '}
-          <A
-            href="https://docs.plural.sh/getting-started/manage-git-repositories/workspace-encryption"
-            target="_blank"
-            inline
-          >
-            Learn more
-          </A>
-        </P>
+          <CreateDummyKeyButton />
+          <CreateKeyButton />
+        </Flex>
       </PageTitle>
-      <Box fill>
-        <P
-          marginBottom="medium"
-          maxWidth={400}
-        >
-          Secure cloud backups of the machine-local AES encryption keys for your
-          installation repositories.{' '}
-          <A
-            href="https://docs.plural.sh/getting-started/manage-git-repositories/workspace-encryption"
-            target="_blank"
-            inline
-          >
-            Learn more
-          </A>
-        </P>
+      <Flex overflow="hidden">
         {!isEmpty(keyBackups) ? (
           <Table
             loose
             data={keyBackups}
-            columns={memoizedCols}
+            columns={columns}
+            maxHeight="100%"
           />
         ) : (
-          <EmptyState message="You do not have any encyrption key backups yet." />
+          <EmptyState message="You do not have any encryption key backups yet." />
         )}
-      </Box>
-    </Box>
+      </Flex>
+    </Flex>
   )
 }

0237660f53bb645e4ba283a85b391c6d3ee0f304
Klink <85062+dogmar@users.noreply.github.com>
2023-04-03 13:09:39 -0700

    Enable running with dev api without changing code

--- www/src/helpers/hostname.ts
+++ www/src/helpers/hostname.ts
@@ -23,7 +23,7 @@ export function apiHost() {
   const { location: { hostname } } = window
 
   if (hostname === 'localhost' || hostname.endsWith('web.app')) {
-    return 'app.plural.sh'
+    return import.meta.env.VITE_API_HOST || 'app.plural.sh'
   }
 
   return hostname

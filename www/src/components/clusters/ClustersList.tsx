fe3de270a21ed85a743107248ef0c1e56cd29a76
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-04-03 11:14:11 +0200

    Add mock cluster empty state

--- www/src/components/clusters/ClustersList.tsx
+++ www/src/components/clusters/ClustersList.tsx
@@ -3,6 +3,8 @@ import {
   Button,
   CaretRightIcon,
   Chip,
+  ClusterIcon,
+  GitHubIcon,
   IconFrame,
   Table,
   TerminalIcon,
@@ -10,6 +12,7 @@ import {
 import { ComponentProps, memo, useMemo } from 'react'
 import { createColumnHelper } from '@tanstack/react-table'
 import styled from 'styled-components'
+import isEmpty from 'lodash/isEmpty'
 
 import { Cluster, Provider, Source } from '../../generated/graphql'
 import CopyButton from '../utils/CopyButton'
@@ -32,6 +35,7 @@ type ClusterListElement = {
       avatar?: string | null
       hasShell?: boolean | null
     }
+    mock?: boolean
   }
 
 const sourceDisplayNames = {
@@ -46,6 +50,8 @@ const CellWrap = styled.div(({ theme }) => ({
   gap: theme.spacing.small,
 }))
 
+const ActionsWrap = styled(CellWrap)({ alignSelf: 'end' })
+
 const CellCaption = styled.div(({ theme }) => ({
   ...theme.partials.text.caption,
   color: theme.colors['text-xlight'],
@@ -57,27 +63,29 @@ export const ColCluster = columnHelper.accessor(row => row.name, {
   id: 'cluster',
   enableGlobalFilter: true,
   enableSorting: true,
-  cell: ({ row: { original } }) => {
-    const cluster = original
-
-    return (
-      <CellWrap>
-        <AppIcon
-          size="xxsmall"
-          icon={(
-            <ProviderIcon
-              provider={cluster.provider}
-              width={16}
-            />
-          )}
-        />
-        <div>
-          <div>{cluster?.name}</div>
-          <CellCaption>{sourceDisplayNames[cluster?.source || '']}</CellCaption>
-        </div>
-      </CellWrap>
-    )
-  },
+  cell: ({
+    row: {
+      original: {
+        name, provider, source, mock,
+      },
+    },
+  }) => (
+    <CellWrap>
+      <AppIcon
+        size="xxsmall"
+        icon={mock ? <ClusterIcon color="icon-warning" /> : (
+          <ProviderIcon
+            provider={provider}
+            width={16}
+          />
+        )}
+      />
+      <div>
+        <div>{name}</div>
+        <CellCaption>{mock ? 'Space' : sourceDisplayNames[source || '']}</CellCaption>
+      </div>
+    </CellWrap>
+  ),
   header: 'Cluster',
 })
 
@@ -85,7 +93,9 @@ export const ColHealth = columnHelper.accessor(row => row.pingedAt, {
   id: 'health',
   enableGlobalFilter: true,
   enableSorting: true,
-  cell: props => <ClusterHealth pingedAt={props.getValue()} />,
+  cell: ({ row: { original: { pingedAt, mock } } }) => (mock
+    ? <Chip severity="success">Enchanted</Chip>
+    : <ClusterHealth pingedAt={pingedAt} />),
   header: 'Health',
 })
 
@@ -93,7 +103,15 @@ export const ColGit = columnHelper.accessor(row => row.gitUrl, {
   id: 'git',
   enableGlobalFilter: true,
   enableSorting: true,
-  cell: props => <CopyButton text={props.getValue() || ''} />,
+  cell: ({ row: { original: { gitUrl, mock } } }) => (mock
+    ? (
+      <IconFrame
+        icon={<GitHubIcon />}
+        textValue=""
+        type="floating"
+      />
+    )
+    : <CopyButton text={gitUrl || ''} />),
   header: 'Git',
 })
 
@@ -101,14 +119,14 @@ export const ColCloudshell = columnHelper.accessor(row => row.owner?.hasShell, {
   id: 'cloudshell',
   enableGlobalFilter: true,
   enableSorting: true,
-  cell: props => (props.getValue()
+  cell: ({ row: { original: { mock, owner } } }) => ((owner?.hasShell || mock)
     ? (
       <IconFrame
-        clickable
+        clickable={!mock}
         icon={<TerminalIcon />}
         onClick={() => null} // TODO: Navigate to cloudshell.
         textValue="Go to cloudshell"
-        tooltip
+        tooltip={!mock}
         type="floating"
       />
     )
@@ -120,23 +138,20 @@ export const ColOwner = columnHelper.accessor(row => row.owner?.name, {
   id: 'owner',
   enableGlobalFilter: true,
   enableSorting: true,
-  cell: ({ row: { original } }) => {
-    const { owner } = original
-
-    return (
-      <CellWrap>
-        <AppIcon
-          name={owner?.name}
-          url={owner?.avatar || undefined}
-          size="xxsmall"
-        />
-        <div>
-          <div>{owner?.name}</div>
-          <CellCaption>{owner?.email}</CellCaption>
-        </div>
-      </CellWrap>
-    )
-  },
+  cell: ({ row: { original: { owner, mock } } }) => (
+    <CellWrap>
+      <AppIcon
+        name={owner?.name}
+        url={owner?.avatar || undefined}
+        size="xxsmall"
+        spacing={mock ? 'none' : undefined}
+      />
+      <div>
+        <div>{owner?.name}</div>
+        <CellCaption>{owner?.email}</CellCaption>
+      </div>
+    </CellWrap>
+  ),
   header: 'Owner',
 })
 
@@ -144,8 +159,8 @@ export const ColUpgrades = columnHelper.accessor(row => row.delivered, {
   id: 'upgrades',
   enableGlobalFilter: true,
   enableSorting: true,
-  cell: props => {
-    const delivered = props.getValue()
+  cell: ({ row: { original: { delivered, mock } } }) => {
+    if (mock) return <Chip severity="warning">Magical</Chip>
 
     return (
       <Chip
@@ -163,39 +178,56 @@ export const ColActions = columnHelper.accessor(row => row.consoleUrl, {
   id: 'actions',
   enableGlobalFilter: false,
   enableSorting: false,
-  meta: {
-    gridTemplate: '200px',
-  },
-  cell: props => {
-    const consoleUrl = props.getValue()
-
-    return (
-      <CellWrap>
-        {consoleUrl && (
-          <Button
-            secondary
-            small
-            as="a"
-            href={consoleUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Launch Console
-          </Button>
-        )}
-        <IconFrame
-          clickable
-          size="medium"
-          icon={<CaretRightIcon />}
-          onClick={() => null} // TODO: Navigate to details page.
-          textValue="Forward"
-          type="tertiary"
-        />
-      </CellWrap>
-    )
-  },
+  cell: ({ row: { original: { consoleUrl, mock } } }) => (
+    <ActionsWrap>
+      {mock && (
+        <Button
+          secondary
+          small
+          as="a"
+          href="https://www.plural.sh/demo-login"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View live demo console
+        </Button>
+      )}
+      {consoleUrl && (
+        <Button
+          secondary
+          small
+          as="a"
+          href={consoleUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Launch Console
+        </Button>
+      )}
+      <IconFrame
+        clickable={!mock}
+        size="medium"
+        icon={<CaretRightIcon />}
+        onClick={() => null} // TODO: Navigate to details page.
+        textValue=""
+        type="tertiary"
+      />
+    </ActionsWrap>
+  ),
   header: '',
 })
+
+const emptyTableData: ClusterListElement[] = [{
+  name: 'Not a cluster, yet...',
+  delivered: false,
+  provider: Provider.Custom,
+  owner: {
+    name: 'Singular',
+    email: 'singular@plural.sh',
+    avatar: '/singular.svg',
+  },
+  mock: true,
+}]
 
 type ClustersListProps = Omit<ComponentProps<typeof Table>, 'data'> & {
     clusters?: (Cluster | null)[]
@@ -232,13 +264,9 @@ export const ClustersList = memo(({ clusters, columns, ...props }: ClustersListP
     }),
   [clusters])
 
-  if (!clusters || clusters.length === 0) {
-    return <>No clusters available.</>
-  }
-
   return (
     <Table
-      data={tableData}
+      data={isEmpty(clusters) ? tableData : emptyTableData}
       columns={columns}
       virtualizeRows
       {...props}

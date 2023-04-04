20a1a4fdcc93c2320da46854e11d10b12900d5bd
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-04-04 10:57:30 +0200

    Add caches for service account impersonation

--- www/src/components/clusters/ImpersonateServiceAccount.tsx
+++ www/src/components/clusters/ImpersonateServiceAccount.tsx
@@ -1,40 +1,47 @@
-import { useEffect, useMemo, useState } from 'react'
-import { ApolloProvider, useMutation } from '@apollo/client'
+import { useEffect, useState } from 'react'
+import { ApolloClient, ApolloProvider, useMutation } from '@apollo/client'
+
+import { memoize } from 'lodash'
+
+import styled from 'styled-components'
 
 import { buildClient } from '../../helpers/client'
 import { IMPERSONATE_SERVICE_ACCOUNT } from '../account/queries'
+import LoadingIndicator from '../utils/LoadingIndicator'
 
-const SA_CLIENT_CACHE = {} // TODO: Change it.
+// TODO: Make sure that below caches are properly destroyed on logout and invalidated after some time.
 
-export function ImpersonateServiceAccount({ id, children }) { // TODO: Loading, errors etc.
-  const [token, setToken] = useState('')
-  const [mutation, { error }] = useMutation(IMPERSONATE_SERVICE_ACCOUNT, {
-    variables: { id },
-    update: (_cache, { data: { impersonateServiceAccount: { jwt } } }) => {
-      console.log(jwt)
-      setToken(jwt) // TODO: Save it in cache, not just current value.
-    },
-  })
+// Cache tokens with service account ID as keys.
+const getImpersonatedToken = memoize((id, mutation) => mutation()
+  .then(({ data: { impersonateServiceAccount: { jwt } } }) => jwt))
+
+// Cache clients with impersonated service account tokens as keys.
+const getClient = memoize(token => buildClient(() => token).client)
+
+const Error = styled.div(({ theme }) => ({
+  color: theme.colors['text-xlight'],
+  padding: `${theme.spacing.small}px ${theme.spacing.medium}px`,
+}))
+
+export function ImpersonateServiceAccount({ id, children }) {
+  const [client, setClient] = useState<ApolloClient<unknown> | undefined>()
+  const [mutation, { error }] = useMutation(IMPERSONATE_SERVICE_ACCOUNT, { variables: { id } })
 
   useEffect(() => {
-    mutation()
+    // Reset client on each service account ID change.
+    // It will be set once matching client will be retrieved.
+    setClient(undefined)
+
+    // Retrieve client matching given service account ID.
+    getImpersonatedToken(id, mutation).then(jwt => setClient(getClient(jwt)))
   }, [id]) // eslint-disable-line react-hooks/exhaustive-deps
 
-  const { client } = useMemo(() => {
-    if (SA_CLIENT_CACHE[token]) return SA_CLIENT_CACHE[token]
-
-    const c = buildClient(() => token)
-
-    SA_CLIENT_CACHE[token] = c
-
-    return c
-  }, [token])
-
-  if (error) return <>Error: {error.message}</>
+  if (error) return <Error>Error while impersonating service account: {error.message}</Error>
+  if (!client) return <LoadingIndicator />
 
   return (
     <ApolloProvider client={client}>
-      {children}
+      { children }
     </ApolloProvider>
   )
 }

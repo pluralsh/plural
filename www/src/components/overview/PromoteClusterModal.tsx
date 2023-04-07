629edf6729ad1db79cb12138425684ac1536da37
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-04-07 16:09:42 +0200

    Restrict promotions to same provider

--- www/src/components/overview/PromoteClusterModal.tsx
+++ www/src/components/overview/PromoteClusterModal.tsx
@@ -55,7 +55,7 @@ export function PromoteClusterModal({ open, setOpen }) {
         <ClusterPicker
           cluster={toCluster}
           setCluster={setToCluster}
-          filter={cluster => cluster?.id !== fromCluster?.id}
+          filter={({ id, provider }: Cluster) => id !== fromCluster?.id && provider === fromCluster?.provider}
           heading="Promote app versions to"
           disabled={!fromCluster}
         />

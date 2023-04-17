d1ae5c76a2a0aeffbc32dd69e5764a5a8277b5cf
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-04-17 08:51:10 +0200

    Remove console button from header

--- www/src/components/layout/Header.tsx
+++ www/src/components/layout/Header.tsx
@@ -1,19 +1,8 @@
-import {
-  A,
-  Div,
-  Flex,
-  Img,
-} from 'honorable'
-import { Button, Tooltip } from '@pluralsh/design-system'
+import { Div, Flex, Img } from 'honorable'
 import { ThemeContext } from 'styled-components'
-import { useQuery } from '@apollo/client'
-
 import { useContext } from 'react'
 
-import { QueueList } from '../overview/Overview'
-import { QUEUES } from '../overview/queries'
 import { useIsCurrentlyOnboarding } from '../shell/hooks/useOnboarded'
-
 import BillingLegacyUserMessage from '../account/billing/BillingLegacyUserMessage'
 import BillingSubscriptionChip from '../account/billing/BillingSubscriptionChip'
 
@@ -22,48 +11,6 @@ const APP_ICON = '/app-logo-white.png'
 export default function Header() {
   const theme = useContext(ThemeContext)
   const isCurrentlyOnboarding = useIsCurrentlyOnboarding()
-
-  const { data: queuesData } = useQuery<QueueList>(QUEUES, {
-    fetchPolicy: 'cache-and-network',
-    pollInterval: 10 * 60 * 1000 /* check every 10 min */,
-  })
-
-  const consoleDomain = queuesData?.upgradeQueues?.[0]?.domain
-  const consoleLink = consoleDomain ? `https://${consoleDomain}` : null
-
-  let consoleButton = (
-    <Button
-      data-phid="nav-launch-console"
-      small
-      {...(consoleLink
-        ? {
-          as: A,
-          href: consoleLink,
-          target: '_blank',
-          rel: 'noopener noreferrer',
-          textDecoration: 'none !important',
-        }
-        : { disabled: true })}
-    >
-      Launch Console
-    </Button>
-  )
-
-  // Don't show tooltip unless data has actually loaded confirming there is
-  // no console to link to.
-  if (queuesData && !consoleLink) {
-    consoleButton = (
-      <Tooltip
-        placement="bottom-end"
-        width={240}
-        label={
-          <>You must have Plural Console deployed to access your console.</>
-        }
-      >
-        <div>{consoleButton}</div>
-      </Tooltip>
-    )
-  }
 
   return (
     <Div
@@ -87,7 +34,6 @@ export default function Header() {
         <Flex grow={1} />
         <BillingLegacyUserMessage />
         <BillingSubscriptionChip />
-        {consoleButton}
       </Flex>
     </Div>
   )

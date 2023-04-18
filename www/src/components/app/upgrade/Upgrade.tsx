f3e8e8539cab8c4766435595e964f04099d04b89
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-04-18 09:47:38 +0200

    Update upgrade channel view

--- www/src/components/app/upgrade/Upgrade.tsx
+++ www/src/components/app/upgrade/Upgrade.tsx
@@ -2,36 +2,29 @@ import {
   Button,
   Card,
   Divider,
+  ListBoxItem,
   PageTitle,
-  Radio,
-  RadioGroup,
+  Select,
+  Tooltip,
+  WrapWithIf,
 } from '@pluralsh/design-system'
-import { P } from 'honorable'
-import { capitalize } from 'lodash'
-import { useCallback, useState } from 'react'
+import { P, Switch } from 'honorable'
+import { capitalize, isEmpty } from 'lodash'
+import { useState } from 'react'
 import { useMutation } from '@apollo/client'
 
 import { UPDATE_INSTALLATION } from '../../repository/queries'
 import { useAppContext } from '../../../contexts/AppContext'
+import { GqlError } from '../../utils/Alert'
 
 export function Upgrade() {
   const { installation, upgradeChannels = [] } = useAppContext()
   const [autoUpgrade, setAutoUpgrade] = useState(installation?.autoUpgrade || false)
-  const [trackTag, setTrackTag] = useState(installation?.trackTag || '')
-  const [mutation, { loading }] = useMutation(UPDATE_INSTALLATION, {
+  const [trackTag, setTrackTag] = useState(installation?.trackTag)
+  const [mutation, { loading, error }] = useMutation(UPDATE_INSTALLATION, {
     variables: { id: installation?.id, attributes: { trackTag, autoUpgrade } },
     onCompleted: () => null,
   })
-
-  const doSetTrackTag = useCallback(tag => {
-    if (tag === 'none') {
-      setAutoUpgrade(false)
-
-      return
-    }
-    setAutoUpgrade(true)
-    setTrackTag(tag)
-  }, [setAutoUpgrade, setTrackTag])
 
   return (
     <>
@@ -59,41 +52,50 @@ export function Upgrade() {
         >
           Determine how this application is updated on a regular basis.
         </P>
-        <RadioGroup
-          direction="row"
-          gap="medium"
+        <WrapWithIf
+          wrapper={<Tooltip label="No upgrade channels available." />}
+          condition={isEmpty(upgradeChannels)}
         >
-          {upgradeChannels?.flatMap(t => (t ? [(
-            <Radio
-              key={t}
-              value={t}
-              checked={trackTag === t && autoUpgrade}
-              onChange={({ target: { checked } }: any) => checked && doSetTrackTag(t)}
-            >
-              {capitalize(t)}
-            </Radio>
-          )] : []))}
-          <Radio
-            value="none"
+          <Switch
             checked={!autoUpgrade}
-            onChange={({ target: { checked } }: any) => checked && doSetTrackTag('none')}
+            onChange={({ target: { checked } }) => {
+              setAutoUpgrade(!checked)
+              if (checked) setTrackTag(undefined)
+            }}
+            disabled={isEmpty(upgradeChannels)}
+            marginBottom="xlarge"
           >
             None
-          </Radio>
-        </RadioGroup>
-        {/* {error && (
+          </Switch>
+        </WrapWithIf>
+        {autoUpgrade && upgradeChannels && !isEmpty(upgradeChannels) && (
+          <Select
+            label="Select upgrade channel"
+            selectedKey={trackTag}
+            onSelectionChange={t => setTrackTag(`${t}`)}
+          >
+            {upgradeChannels.flatMap(t => (t ? [(
+              <ListBoxItem
+                key={t}
+                label={capitalize(t)}
+                textValue={t}
+              />
+            )] : []))}
+          </Select>
+        )}
+        {error && (
           <GqlError
             error={error}
             header="Failed to uninstall"
           />
-        )} */}
+        )}
         <Divider
           backgroundColor="border"
           marginVertical="xlarge"
         />
         <Button
           onClick={mutation}
-          // disabled={confirm !== app.name}
+          disabled={autoUpgrade && isEmpty(trackTag)}
           loading={loading}
           alignSelf="end"
           width="max-content"

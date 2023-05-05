af68c7e295b1f2ef3c248bf253f9b5b49fb86642
Klink <85062+dogmar@users.noreply.github.com>
2023-05-05 09:45:39 -0700

    lint

--- www/src/serviceWorkerRegistration.ts
+++ www/src/serviceWorkerRegistration.ts
@@ -58,9 +58,7 @@ const onLoad = (config) => async () => {
   // Add some additional logging to localhost, pointing developers to the
   // service worker/PWA documentation.
   navigator.serviceWorker.ready.then(() => {
-    console.log(
-      'This web app is being served cache-first by a service ' + 'worker.'
-    )
+    console.log('This web app is being served cache-first by a service worker.')
   })
 }
 

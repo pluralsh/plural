6359f1ef1074767d75280cc88faf22d1c6754549
michaeljguarino <mguarino46@gmail.com>
2023-05-08 16:07:30 -0500

    Rename warm to stable
    The nomenclature has confused some people, so this might help.  We're also only going to maintain latest and stable from now on as well

--- apps/core/test/backfill/repositories_test.exs
+++ apps/core/test/backfill/repositories_test.exs
@@ -12,4 +12,23 @@ defmodule Core.Backfill.RepositoriesTest do
         do: assert is_binary(refetch(inst).license_key)
     end
   end
+
+  describe "#warm_to_stable" do
+    test "it can move all warm tags to stable" do
+      vts = insert_list(3, :version_tag, tag: "warm")
+      ignore = insert(:version_tag, tag: "latest")
+      inst = insert(:installation, track_tag: "warm")
+      inst2 = insert(:installation, track_tag: "latest")
+
+      Repositories.warm_to_stable()
+
+      for vt <- vts,
+        do: assert refetch(vt).tag == "stable"
+
+      assert refetch(ignore).tag == "latest"
+
+      assert refetch(inst).track_tag == "stable"
+      assert refetch(inst2).track_tag == "latest"
+    end
+  end
 end

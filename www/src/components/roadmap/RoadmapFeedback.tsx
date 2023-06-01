f9cb38c4fb1b52cbce0902407fc90feb90565748
Klink <85062+dogmar@users.noreply.github.com>
2023-06-01 08:42:15 -0700

    fix: Make Roadmap search inputs maintain full height (#1112)

--- www/src/components/roadmap/RoadmapFeedback.tsx
+++ www/src/components/roadmap/RoadmapFeedback.tsx
@@ -8,19 +8,22 @@ function RoadmapFeedback() {
       <P
         body2
         color="text-xlight"
+        marginBottom="small"
       >
         Hearing your feedback is important to us and helps shape our roadmap and
         make our product better.
       </P>
-      <A
-        inline
-        display="block"
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://discord.gg/pluralsh"
-      >
-        Give us feedback on discord.
-      </A>
+      <P>
+        <A
+          inline
+          display="block"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://discord.gg/pluralsh"
+        >
+          Give us feedback on discord.
+        </A>
+      </P>
     </ScrollablePage>
   )
 }

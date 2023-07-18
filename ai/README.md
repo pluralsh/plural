ef64eea478b36d045033bb46278be3b55974af6a
michaeljguarino <mguarino46@gmail.com>
2023-07-18 12:24:08 -0400

    Dockerize external ai contribution (#1163)

---
+++ ai/README.md
@@ -0,0 +1,10 @@
+# pluralai
+Plural.sh AI is built with FastAPI
+
+- Install the requirements.txt by running `pip install -r requirments.txt`
+- Start the server by running `uvicorn main:app --reload`
+
+Swaggger Documentation: /docs
+Chat endpoint: /chat
+
+Export your openai key with `export OPENAI_API_KEY = 'sk-yourkey'`
---
+++ ai/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, HTTPException
+from llama_index import StorageContext, load_index_from_storage
+from llama_index.indices.postprocessor import SentenceEmbeddingOptimizer
+
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class QueryRequest(BaseModel):
+    question: str
+
+class QueryResponse(BaseModel):
+    answer: str
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/chat")
+def query_data(request: QueryRequest):
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    index = load_index_from_storage(storage_context)
+    query_engine = index.as_query_engine(
+        node_postprocessors=[SentenceEmbeddingOptimizer(percentile_cutoff=0.5)],    
+        response_mode="compact",
+        similarity_cutoff=0.7
+    )
+    response = query_engine.query(request.question)
+
+    if not response:
+        raise HTTPException(status_code=404, detail="No results found")
+
+    return QueryResponse(answer=str(response))

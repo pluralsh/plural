f53dea92e334dd032a7b44266fafec2901c7c62c
michaeljguarino <mguarino46@gmail.com>
2023-07-20 09:20:54 -0400

    Enhance plural ai scraper (#1167)

--- ai/README.md
+++ ai/README.md
@@ -1,10 +1,35 @@
 # pluralai
-Plural.sh AI is built with FastAPI
+This using the python llama index project to manage most of the embedding and storage, which has plugins for most of the stuff we need.  There is both a scraper and a webserver.  Run instructions are available for both.
 
-- Install the requirements.txt by running `pip install -r requirments.txt`
-- Start the server by running `uvicorn main:app --reload`
+To do either, you'll need to install `requirements.txt` with:
+
+```sh
+pip install -r requirments.txt
+```
+
+## Running the webserver
+The server is built with FastAPI. To start the server by running `uvicorn main:app --reload`
 
 Swaggger Documentation: /docs
 Chat endpoint: /chat
 
-Export your openai key with `export OPENAI_API_KEY = 'sk-yourkey'`
+## Running scraper.py
+
+The scraper currently incorporates three datasources:
+
+* the sitemap from docs.plural.sh
+* the app specific docs from the plural gql api
+* discord message history
+
+You'll need an openai api key to generate embeddings, and to scrape discord (optional) you'll also need a discord token.  Export both with:
+
+```sh
+export OPENAI_API_KEY=an-openai-key
+export DISCORD_TOKEN=a-discord-token
+```
+
+You should then be able to run:
+
+```sh
+./scraper.py
+```
--- ai/scraper.py
+++ ai/scraper.py
@@ -1,89 +1,120 @@
+#!python3
+import os
+import html2text
 import requests
-from bs4 import BeautifulSoup
+import itertools
+import openai
 import json
 import xml.etree.ElementTree as ET
+import yaml
+from yaml.loader import SafeLoader
+from python_graphql_client import GraphqlClient
+from llama_index import Document, VectorStoreIndex, BeautifulSoupWebReader, DiscordReader, ServiceContext, set_global_service_context
+from llama_index.embeddings import OpenAIEmbedding
 
-# Fetch the sitemap XML content
-sitemap_url = "https://docs.plural.sh/sitemap.xml"
-response = requests.get(sitemap_url)
-sitemap_content = response.text
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# Parse the XML content
-root = ET.fromstring(sitemap_content)
+def get_token():
+    if os.environ.get('PLURAL_ACCESS_TOKEN'):
+        return os.environ['PLURAL_ACCESS_TOKEN']
 
-# Find all <loc> elements and extract the link URLs
-urls = []
-for loc_elem in root.iter("{http://www.sitemaps.org/schemas/sitemap/0.9}loc"):
-    link = loc_elem.text
-    urls.append(link)
+    with open(os.path.expanduser("~/.plural/config.yml")) as f:
+        data = yaml.load(f, Loader=SafeLoader)
+        return data["spec"]["token"]
 
+def gql_client():
+    token = get_token()
+    return GraphqlClient(endpoint="https://app.plural.sh/gql", headers={"Authorization": f"Bearer {token}"})
 
-docs_text = []
+fetch_repos = """
+query Repos($cursor: String) {
+    repositories(first: 100, after: $cursor) {
+        pageInfo { hasNextPage endCursor }
+        edges { node { id name } }
+    }
+}
+"""
 
-# Iterate over each page URL
-for i, url in enumerate(urls):
-    try:
-        # Fetch the HTML content
-        response = requests.get(url)
-        html_content = response.content
+fetch_docs = """
+query Repo($id: ID!) {
+    repository(id: $id) {
+        readme
+        gitUrl
+        docs { path content }
+    }
+}
+"""
 
-        # Parse the HTML using BeautifulSoup
-        soup = BeautifulSoup(html_content, "html.parser")
+def document(text, **kwargs):
+    return Document(text=text, metadata=kwargs)
 
-        # Extract the page title
-        title = soup.title.string.strip().replace("Docs | Plural |", "") if soup.title else "No Title"
+def scrape_app_docs():
+    api = gql_client()
 
-        # Find the target div element
-        div = soup.find("div", class_="sc-520f8824-0 jyoUZy")
+    def list_repos(cursor):
+        result = api.execute(query=fetch_repos, variables={"cursor": cursor})
+        return result["data"]["repositories"]["edges"], result["data"]["repositories"]["pageInfo"]
+    
+    has_next, cursor = True, None
+    while has_next:
+        edges, page_info = list_repos(cursor)
+        has_next, cursor = page_info["hasNextPage"], page_info["endCursor"]
+        for node in edges:
+            repo_name = node["node"]["name"]
+            print(f"fetching docs for {repo_name}")
+            try:
+                result = api.execute(query=fetch_docs, variables={"id": node["node"]["id"]})
+                repo = result["data"]["repository"]
+                if repo.get("readme") and repo.get("gitUrl"):
+                    yield document(repo["readme"], page_link=repo["gitUrl"], title=f"{repo_name} readme")
 
-        if div:
-            # Extract the desired content
-            subtitle = div.find("p").text.strip()
-            content_elements = div.find_all(["h1", "h2", "h3", "h4", "h5", "h6", "p", "ul", "pre", "img", "a"])
+                for doc in repo["docs"]:
+                    yield document(doc["content"], page_link=doc["path"], title=os.path.basename(doc["path"].rstrip(".md")))
+            except Exception as e:
+                print(f"Failed to scrape repository: {repo_name}")
+                print(f"Error: {str(e)}\n")
 
-            # Convert content elements to markdown format
-            markdown_content = []
-            for element in content_elements:
-                if element.name.startswith("h"):
-                    heading_level = int(element.name[1])
-                    heading_text = element.text.strip()
-                    markdown_content.append("#" * heading_level + " " + heading_text)
-                elif element.name == "pre":
-                    code_block = element.text.strip()
-                    markdown_content.append(f"```\n{code_block}\n```")
-                elif element.name == "img":
-                    image_src = element["src"]
-                    markdown_content.append(f"![Image]({image_src})")
-                elif element.name == "a":
-                    link_href = element["href"]
-                    link_text = element.text.strip()
-                    markdown_content.append(f"[{link_text}]({link_href})")
-                else:
-                    paragraph_text = element.text.strip()
-                    markdown_content.append(paragraph_text)
+def _docs_reader(soup):
+    """Extract text from Substack blog post."""
+    metadata = {
+        "title": soup.title.string.strip().replace("Docs | Plural |", "") if soup.title else "No Title",
+    }
 
-            # Convert markdown content to a single string
-            markdown_text = "\n\n".join(markdown_content)
+    def to_markdown(soup):
+        return html2text.html2text(str(soup))
 
-            # Create a dictionary for the scraped data
-            doc_data = {
-                "id": i + 1,
-                "page_link": url,
-                "title": title,
-                "text": markdown_text,
-                "source_links": [],
-            }
+    div = soup.find("div", class_="sc-520f8824-0 jyoUZy")
+    if div:
+        return to_markdown(div), metadata
+    return to_markdown(soup), metadata
 
-            # Append the dictionary to the list
-            docs_text.append(doc_data)
+def scrape_plural_docs():
+    sitemap_url = "https://docs.plural.sh/sitemap.xml"
+    response = requests.get(sitemap_url)
+    sitemap_content = response.text
 
-    except Exception as e:
-        print(f"Failed to scrape page: {url}")
-        print(f"Error: {str(e)}")
-        print()
+    root = ET.fromstring(sitemap_content)
+    reader = BeautifulSoupWebReader(website_extractor={"docs.plural.sh": _docs_reader})
+    return reader.load_data([loc.text for loc in root.iter("{http://www.sitemaps.org/schemas/sitemap/0.9}loc")])
 
-# Write the scraped data to a JSON file
-with open("scraped_data.json", "w") as file:
-    json.dump(docs_text, file, indent=4)
+def scrape_discord():
+    discord_token = os.getenv("DISCORD_TOKEN")
+    channel_ids = [880837182389108766]  # Replace with your channel_id
+    return DiscordReader(discord_token=discord_token).load_data(
+        channel_ids=channel_ids,
+        limit=5000,
+        oldest_first=False
+    )
 
-print("Scraped data saved to 'scraped_data.json'")
+embed_model = OpenAIEmbedding(embed_batch_size=10)
+service_context = ServiceContext.from_defaults(embed_model=embed_model)
+set_global_service_context(service_context)
+
+chain = itertools.chain(scrape_app_docs(), scrape_plural_docs())
+if os.getenv("DISCORD_TOKEN"):
+    chain = itertools.chain(chain, scrape_discord())
+
+index = VectorStoreIndex.from_documents(list(chain))
+index.storage_context.persist()
+
+print("persisted new vector index")

3bf7c8151d087a3af2114a9600b8f176f3444eee
michaeljguarino <mguarino46@gmail.com>
2023-08-07 09:34:22 -0400

    Forcibly recompute usage before syncing
    This will simply ensure that we have a correct latest value to apply on a users bill.

--- apps/cron/lib/cron/task/usage.ex
+++ apps/cron/lib/cron/task/usage.ex
@@ -4,7 +4,7 @@ defmodule Cron.Task.Usage do
   """
   use Cron
   alias Core.Schema.Account
-  alias Core.Services.Payments
+  alias Core.Services.{Payments, Accounts}
 
   def run() do
     Account.usage_updated()
@@ -14,4 +14,10 @@ defmodule Cron.Task.Usage do
     |> Stream.map(&Payments.sync_usage/1)
     |> log()
   end
+
+  defp sync_usage(%Account{} = account) do
+    with {:ok, account} <- Accounts.recompute_usage(account),
+      do: Payments.sync_usage(account)
+  end
+  defp sync_usage(_), do: :ok
 end

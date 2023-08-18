d90bb92002f45d873cc0235187a96fcecf30db5f
michaeljguarino <mjg@plural.sh>
2023-08-18 12:14:02 -0400

    Wire in appropriate toleration and node selectors for sysbox (#1205)

--- apps/core/test/services/shell/pods_test.exs
+++ apps/core/test/services/shell/pods_test.exs
@@ -7,12 +7,22 @@ defmodule Core.Shell.PodsTest do
       pod = Pods.pod("plrl-shell-1", "mjg@plural.sh")
 
       refute pod.spec.runtime_class_name
+      assert pod.spec.node_selector["platform.plural.sh/instance-class"] == "shell"
+
+      [toleration] = pod.spec.tolerations
+      assert toleration.key == "platform.plural.sh/taint"
+      assert toleration.value == "SHELL"
     end
 
     test "a whitelisted user gets the sysbox class" do
       pod = Pods.pod("plrl-shell-1", "sysbox@plural.sh")
 
       assert pod.spec.runtime_class_name == "sysbox"
+      assert pod.spec.node_selector["sysbox-runtime"] == "running"
+
+      [toleration] = pod.spec.tolerations
+      assert toleration.key == "plural.sh/sysbox"
+      assert toleration.value == "true"
     end
   end
 end

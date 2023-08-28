e6099625b46041c5c7aa429e954defdb00cd803f
michaeljguarino <mguarino46@gmail.com>
2023-08-28 09:47:05 -0400

    Add immediate email for promotions
    Also enable the digest cron written a while ago but never wired up

--- apps/email/lib/email/deliverable/versions.ex
+++ apps/email/lib/email/deliverable/versions.ex
@@ -1,3 +1,8 @@
 defimpl Email.Deliverable, for: Core.PubSub.InstallationLocked do
   def email(%{item: inst}), do: Email.Builder.LockedInstallation.email(inst)
 end
+
+defimpl Email.Deliverable, for: Core.PubSub.DeferredUpdateCreated do
+  def email(%{item: %{pending: true} = update}), do: Email.Builder.PendingPromotion.email(update)
+  def email(_), do: :ok
+end

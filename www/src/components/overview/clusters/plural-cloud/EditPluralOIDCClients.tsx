1a109955369adbcfc1657cde66dfa13a758183dd
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-10-17 12:23:13 +0200

    add oidc providers table

--- www/src/components/overview/clusters/plural-cloud/EditPluralOIDCClients.tsx
+++ www/src/components/overview/clusters/plural-cloud/EditPluralOIDCClients.tsx
@@ -1,14 +1,19 @@
 import {
+  AppIcon,
   Button,
+  ConsoleIcon,
   Flex,
   ListBoxItem,
   Modal,
+  PlusIcon,
   Select,
+  Table,
 } from '@pluralsh/design-system'
 import { FormFieldSC } from 'components/create-cluster/steps/ConfigureCloudInstanceStep'
 import {
   ConsoleInstanceFragment,
   ConsoleSize,
+  OidcProviderFragment,
   useGroupMembersQuery,
   useNotificationsQuery,
   useOidcProvidersQuery,
@@ -21,8 +26,27 @@ import { GqlError } from 'components/utils/Alert'
 import { useTheme } from 'styled-components'
 
 import { firstLetterUppercase } from './CloudInstanceTableCols'
-import { useFetchPaginatedData } from '../../../utils/useFetchPaginatedData'
+import {
+  DEFAULT_REACT_VIRTUAL_OPTIONS,
+  useFetchPaginatedData,
+} from '../../../utils/useFetchPaginatedData'
 import { mapExistingNodes } from '../../../../utils/graphql'
+import { createColumnHelper } from '@tanstack/react-table'
+import { CellWrap } from '../SelfHostedTableCols'
+
+const columnHelper = createColumnHelper<OidcProviderFragment>()
+
+const columns = [
+  columnHelper.accessor((row) => row, {
+    id: 'name',
+    enableSorting: true,
+    cell: ({ getValue }) => {
+      const oidcProvider = getValue()
+
+      return <div>{oidcProvider.name}</div>
+    },
+  }),
+]
 
 export function EditPluralOIDCClientsModal({
   open,
@@ -55,13 +79,14 @@ function EditPluralOIDCClients({
   instance: ConsoleInstanceFragment
 }) {
   const theme = useTheme()
-  const { data, loading, pageInfo, fetchNextPage } = useFetchPaginatedData(
-    { queryHook: useOidcProvidersQuery, keyPath: ['oidcProviders'] },
-    {}
-  )
+  const { data, loading, pageInfo, setVirtualSlice, fetchNextPage } =
+    useFetchPaginatedData(
+      { queryHook: useOidcProvidersQuery, keyPath: ['oidcProviders'] },
+      {}
+    )
 
   const oidcProviders = useMemo(
-    () => mapExistingNodes(data?.oidcProviders),
+    () => mapExistingNodes(data?.oidcProviders) ?? [],
     [data?.oidcProviders]
   )
 
@@ -73,7 +98,47 @@ function EditPluralOIDCClients({
         gap: theme.spacing.xlarge,
       }}
     >
-      {JSON.stringify(oidcProviders)}
+      <div
+        css={{
+          border: theme.borders['fill-two'],
+          borderRadius: theme.borderRadiuses.large,
+        }}
+      >
+        <Table
+          virtualizeRows
+          rowBg="raised"
+          data={oidcProviders}
+          columns={columns}
+          hideHeader
+          hasNextPage={pageInfo?.hasNextPage}
+          fetchNextPage={fetchNextPage}
+          isFetchingNextPage={loading}
+          onVirtualSliceChange={setVirtualSlice}
+          reactVirtualOptions={DEFAULT_REACT_VIRTUAL_OPTIONS}
+          style={{
+            border: 'none',
+            borderBottomLeftRadius: 0,
+            borderBottomRightRadius: 0,
+            height: '100%',
+          }}
+        />
+        <div
+          css={{
+            ...theme.partials.text.body2Bold,
+            borderTop: theme.borders['fill-two'],
+            display: 'flex',
+            justifyContent: 'space-between',
+            padding: theme.spacing.medium,
+
+            '&:hover': {
+              backgroundColor: theme.colors['fill-one-hover'],
+              cursor: 'pointer',
+            },
+          }}
+        >
+          Add new Plural OIDC client <PlusIcon color={'icon-light'} />
+        </div>
+      </div>
       <Button
         secondary
         onClick={onClose}

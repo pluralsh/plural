95e90a51489845fc3bfd45f4332e82009c0deae9
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-10-21 11:47:31 +0200

    hide form after creation

--- www/src/components/overview/clusters/plural-cloud/EditPluralOIDCClient.tsx
+++ www/src/components/overview/clusters/plural-cloud/EditPluralOIDCClient.tsx
@@ -222,121 +222,125 @@ function EditPluralOIDCClient({
             {provider?.clientSecret}
           </Codeline>
         </FormField>
-        <Divider
-          backgroundColor="border-fill-two"
-          marginTop="medium"
-          marginBottom="medium"
-        />
-        <FormField
-          label="Name"
-          required
-        >
-          <Input
-            autoFocus
-            value={name}
-            onChange={({ target: { value } }) => setName(value)}
-          />
-        </FormField>
-        <FormField label="Description">
-          <Input
-            value={description}
-            onChange={({ target: { value } }) => setDescription(value)}
-          />
-        </FormField>
-        <BindingInput
-          label="User bindings"
-          placeholder="Search for user"
-          bindings={bindings
-            .filter(({ user }) => !!user)
-            .map(({ user }) => user?.email)}
-          fetcher={fetchUsers}
-          add={(user) => setBindings([...bindings, { user }])}
-          remove={(email) =>
-            setBindings(
-              bindings.filter(({ user }) => !user || user.email !== email)
-            )
-          }
-        />
-        <BindingInput
-          label="Group bindings"
-          placeholder="Search for group"
-          bindings={bindings
-            .filter(({ group }) => !!group)
-            .map(({ group: { name } }) => name)}
-          fetcher={fetchGroups}
-          add={(group) => setBindings([...bindings, { group }])}
-          remove={(name) =>
-            setBindings(
-              bindings.filter(({ group }) => !group || group.name !== name)
-            )
-          }
-        />
-        <FormField
-          label="Auth method"
-          required
-        >
-          <Select
-            selectedKey={authMethod}
-            onSelectionChange={(v) => setAuthMethod(v as OidcAuthMethod)}
-          >
-            <ListBoxItem
-              key={OidcAuthMethod.Basic}
-              label={'Basic'}
-              textValue={OidcAuthMethod.Basic}
+        {!data && (
+          <>
+            <Divider
+              backgroundColor="border-fill-two"
+              marginTop="medium"
+              marginBottom="medium"
             />
-            <ListBoxItem
-              key={OidcAuthMethod.Post}
-              label={'Post'}
-              textValue={OidcAuthMethod.Post}
-            />
-          </Select>
-        </FormField>
-        <FormField label="Redirect URIs">
-          <div
-            css={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: theme.spacing.small,
-            }}
-          >
-            <div css={{ display: 'flex' }}>
-              <Input
-                value={url}
-                prefix={urlPrefix}
-                suffix={urlSuffix}
-                width="100%"
-                placeholder="Enter a redirect URI"
-                onChange={({ target: { value } }) => setUrl(value)}
-              />
-              <Button
-                onClick={addUrl}
-                secondary
-                marginLeft="small"
-              >
-                Add
-              </Button>
-            </div>
-            <div
-              css={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                gap: theme.spacing.xxsmall,
-              }}
+            <FormField
+              label="Name"
+              required
             >
-              {redirectUris.map((url, i) => (
-                <Chip
-                  key={i}
-                  size="small"
-                  clickable
-                  closeButton
-                  onClick={() => removeUrl(url)}
+              <Input
+                autoFocus
+                value={name}
+                onChange={({ target: { value } }) => setName(value)}
+              />
+            </FormField>
+            <FormField label="Description">
+              <Input
+                value={description}
+                onChange={({ target: { value } }) => setDescription(value)}
+              />
+            </FormField>
+            <BindingInput
+              label="User bindings"
+              placeholder="Search for user"
+              bindings={bindings
+                .filter(({ user }) => !!user)
+                .map(({ user }) => user?.email)}
+              fetcher={fetchUsers}
+              add={(user) => setBindings([...bindings, { user }])}
+              remove={(email) =>
+                setBindings(
+                  bindings.filter(({ user }) => !user || user.email !== email)
+                )
+              }
+            />
+            <BindingInput
+              label="Group bindings"
+              placeholder="Search for group"
+              bindings={bindings
+                .filter(({ group }) => !!group)
+                .map(({ group: { name } }) => name)}
+              fetcher={fetchGroups}
+              add={(group) => setBindings([...bindings, { group }])}
+              remove={(name) =>
+                setBindings(
+                  bindings.filter(({ group }) => !group || group.name !== name)
+                )
+              }
+            />
+            <FormField
+              label="Auth method"
+              required
+            >
+              <Select
+                selectedKey={authMethod}
+                onSelectionChange={(v) => setAuthMethod(v as OidcAuthMethod)}
+              >
+                <ListBoxItem
+                  key={OidcAuthMethod.Basic}
+                  label={'Basic'}
+                  textValue={OidcAuthMethod.Basic}
+                />
+                <ListBoxItem
+                  key={OidcAuthMethod.Post}
+                  label={'Post'}
+                  textValue={OidcAuthMethod.Post}
+                />
+              </Select>
+            </FormField>
+            <FormField label="Redirect URIs">
+              <div
+                css={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: theme.spacing.small,
+                }}
+              >
+                <div css={{ display: 'flex' }}>
+                  <Input
+                    value={url}
+                    prefix={urlPrefix}
+                    suffix={urlSuffix}
+                    width="100%"
+                    placeholder="Enter a redirect URI"
+                    onChange={({ target: { value } }) => setUrl(value)}
+                  />
+                  <Button
+                    onClick={addUrl}
+                    secondary
+                    marginLeft="small"
+                  >
+                    Add
+                  </Button>
+                </div>
+                <div
+                  css={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    gap: theme.spacing.xxsmall,
+                  }}
                 >
-                  {url}
-                </Chip>
-              ))}
-            </div>
-          </div>
-        </FormField>
+                  {redirectUris.map((url, i) => (
+                    <Chip
+                      key={i}
+                      size="small"
+                      clickable
+                      closeButton
+                      onClick={() => removeUrl(url)}
+                    >
+                      {url}
+                    </Chip>
+                  ))}
+                </div>
+              </div>
+            </FormField>
+          </>
+        )}
       </div>
       {error && (
         <GqlError
@@ -346,20 +350,32 @@ function EditPluralOIDCClient({
           } OIDC provider request failed`}
         />
       )}
-      <div css={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Button
-          secondary
-          onClick={onClose}
-        >
-          Back to Plural OIDC clients
-        </Button>
-        <Button
-          secondary
-          loading={loading}
-          onClick={mutation}
-        >
-          {createMode ? 'Create' : 'Save'}
-        </Button>
+      <div
+        css={{
+          display: 'flex',
+          justifyContent: 'end',
+          gap: theme.spacing.small,
+        }}
+      >
+        {!data ? (
+          <>
+            <Button
+              secondary
+              onClick={onClose}
+            >
+              Back to Plural OIDC clients
+            </Button>
+            <div css={{ flexGrow: 1 }} />
+            <Button
+              loading={loading}
+              onClick={mutation}
+            >
+              {createMode ? 'Create' : 'Save'}
+            </Button>
+          </>
+        ) : (
+          <Button onClick={onClose}>Close</Button>
+        )}
       </div>
     </div>
   )

7eb3998f980e5647be69fd8ad2a907b2dd507f19
michaeljguarino <mjg@plural.sh>
2025-06-05 17:47:00 -0400

    Fix setup_trial tool (#1426)

--- apps/core/lib/core/mcp/tools/setup_trial.ex
+++ apps/core/lib/core/mcp/tools/setup_trial.ex
@@ -3,7 +3,7 @@ defmodule Core.MCP.Tools.SetupTrial do
   import Core.MCP.Tools.Utils
   alias Core.Services.{Payments}
   alias Core.Repo
-  alias Core.Schema.{Account, PlatformSubscription}
+  alias Core.Schema.{Account, PlatformSubscription, PlatformPlan}
 
   def name(), do: "setup_trial"
 
@@ -24,18 +24,28 @@ defmodule Core.MCP.Tools.SetupTrial do
 
   def invoke(%{"account_id" => id}) do
     with {:account, {:ok, %Account{} = account}} <- {:account, get_account(id)},
-         {:ok, _} <- maybe_setup_trial(Repo.preload(account, [:subscription])) do
-      {:ok, "trial setup for #{id}"}
+         {:plan, %PlatformPlan{} = plan} <- {:plan, get_plan()},
+         {:ok, _} <- maybe_setup_trial(Repo.preload(account, [:subscription]), plan) do
+      {:ok, "trial established for #{id}"}
     else
       {:account, {:error, err}} -> {:ok, err}
       {:account, _} -> {:ok, "no account with id #{id}"}
+      {:plan, _} -> {:ok, "no trial plan defined"}
       err -> err
     end
   end
-
   def invoke(_), do: {:error, "account id is required"}
 
-  defp maybe_setup_trial(%Account{subscription: %PlatformSubscription{}}),
+  defp get_plan() do
+    Core.conf(:trial_plan)
+    |> Payments.get_platform_plan_by_name!()
+  end
+
+  defp maybe_setup_trial(%Account{subscription: %PlatformSubscription{}}, _plan),
     do: {:error, "account already has a subscription"}
-  defp maybe_setup_trial(%Account{} = account), do: Payments.begin_trial(%Account{account | trialed: false})
+  defp maybe_setup_trial(%Account{} = account, %PlatformPlan{id: id}) do
+    %PlatformSubscription{account_id: account.id}
+    |> PlatformSubscription.changeset(%{plan_id: id})
+    |> Core.Repo.insert()
+  end
 end
